da1ac5b4868507f5301cd604cf002c7fc5def24a
Michael McCourt <mccomic@mcs.anl.gov>
2011-07-17 23:15:47 -0500

    Fixed the issue, HermiteProd should be good to go now. This does change what the answer looks like for ex2, which should be expeted to some degree since if the asymptotic appx wasn't needed I wouldn't be doin this.

--- source/HermiteProd.m
+++ source/HermiteProd.m
@@ -1,4 +1,4 @@
-function Hx = HermiteProd(m,x,logoption)
+function [Hx,negInd] = HermiteProd(m,x,logoption)
 % This function takes a vector m and a matrix x
 % [mr mc] = size(m)
 % [xr xc] = size(x)
@@ -8,10 +8,21 @@ function Hx = HermiteProd(m,x,logoption)
 % If logoption is activated, the value returned will be log of Hx
 %   Note that with logoption on you will have complex Hx
 %   You should see real(exp(HermiteProd(m,x,1)))=HermiteProd(m,x) to near machine precision 
+%
+% At some point it will probably be cost effective to exploit the
+% recurrence relation when the log option isn't needed
+% To do so I'll have to change the structure from
+%   Compute each column individually
+% to
+%   Compute 3 or more columns at a time, and use H_n+1 = 2xH_n - 2nH_n-1
+% Doing so wouldn't be appropriate for the log option, I don't think
 
-M = {[1] [2 0] [4 0 -2] [8 0 -12 0] [16 0 -48 0 12] [32 0 -160 0 120 0] [64 0 -480 0 720 0 -120] ...
-     [128 0 -1344 0 3360 0 -1680 0] [256 0 -3584 0 13440 0 -13440 0 1680] [512 0 -9216 0 48384 0 -80640 0 30240 0] ...
-     [1024 0 -23040 0 161280 0 -403200 0 302400 0 -30240]};
+global GAUSSQR_PARAMETERS
+if ~isstruct(GAUSSQR_PARAMETERS)
+    error('GAUSSQR_PARAMETERS does not exist ... did you forget to call rbfsetup?')
+end
+GPHAMI = GAUSSQR_PARAMETERS.HERMITE_ASYMPTOTIC_MIN_INDEX;
+GPHC = GAUSSQR_PARAMETERS.HERMITE_COEFFICIENTS;
 
 [mr mc] = size(m);
 [xr xc] = size(x);
@@ -22,29 +33,35 @@ end
 if mc~=1
     error(sprintf('m must be a column vector: mr=%d, mc=%d',mr,mc))
 end
+
 if nargin==3 && (logoption~=0 || logoption~=false)
     logoption=1;
+    Hx = zeros(xr,1);
 else
     logoption=0;
+    Hx = ones(xr,1);
 end
+negInd = zeros(xr,1);
 
 for k=1:length(m)
-    if m(k)<=length(M)-1
-        H = polyval(M{m(k)+1},x(:,k));
+    if m(k)<GPHAMI
+        if m(k)<=length(GPHC)-1 % To allow for changes on the fly
+            H = polyval(GPHC{m(k)+1},x(:,k));
+        else
+            H = polyval(HermitePoly(m(k)),x(:,k));
+        end
+        if logoption
+            nI = H<0;
+            H = log(abs(H)+eps);
+        end
     else
-        H = polyval(HermitePoly(m(k)),x(:,k));
+        [H,nI] = HermiteAppx(m(k),x(:,k),logoption);
     end
-    if logoption==1
-        if k==1
-            Hx = log(H+eps); % For log(0)
-        else
-            Hx = Hx + log(H+eps); % For log(0)
-        end
+    
+    if logoption
+        Hx = Hx + H;
+        negInd = xor(nI,negInd);
     else
-        if k==1
-            Hx = H;
-        else
-            Hx = Hx.*H;
-        end
+        Hx = Hx.*H;
     end
 end
