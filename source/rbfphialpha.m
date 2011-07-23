87a46fc8ee76ab2a49ce5ab1a68fb07e32e4bfbd
Michael McCourt <mccomic@mcs.anl.gov>
2011-07-23 02:07:12 -0500

    Worked on derivatives for rbfphialpha. Not tested yet. Need to allow for M<1, and also think about how to fix index shift 0->1 everywhere.

--- source/rbfphialpha.m
+++ source/rbfphialpha.m
@@ -1,6 +1,6 @@
-% function p = rbfphialpha(Marr,x,ep,alpha,d,beta,gamma,delta,sx2)
+% function p = rbfphialpha(Marr,x,ep,alpha,deriv,beta,gamma,delta,sx2)
 % This function should in general be used in the following form
-%   function p = rbfphi(Marr,x,ep,a)
+%   function p = rbfphi(Marr,x,ep,alpha)
 % which lets you pass inputs to produce the needed eigenfunctions
 %
 % Marr - array of exponential values for the eigenfunction
@@ -10,11 +10,17 @@
 % x - vector of RBF centers
 % ep - traditional RBF shape parameter
 % alpha - RBFQR global scale parameter
-% d - row vector of derivatives in each dimension
+% deriv - row vector of derivatives in each dimension
 % beta,gamma,delta,sx2 - DO NOT PASS, it is for private use
 %
-% Note: if you pass alpha<0 or ep<0, this will break
-function p = rbfphialpha(Marr,x,ep,alpha,d,beta,delta,sx2)
+% Note: if you pass alpha<0 or ep<0 or m<1, this will error out
+%
+% Derivatives are calculated using the relationship:
+%    d/dx(p_{n}(x)) = 2*delta*x*p_{n}(x)+sqrt(2n-2)*beta*alpha*p_{n-1}(x)
+%
+% Fix: Need to allow for M<1, and just return 0 in that case
+%      Should match with H_0=1, and anything less is 0
+function p = rbfphialpha(Marr,x,ep,alpha,deriv,beta,delta,sx2)
 
 global GAUSSQR_PARAMETERS
 if ~isstruct(GAUSSQR_PARAMETERS)
@@ -28,26 +34,28 @@ asympttol = GAUSSQR_PARAMETERS.RBFPHI_EXP_TOL;
 if Mr~=xc
     error(sprintf('dimension mismatch: Mr=%d, xc=%d',Mr,xc))
 end
-if min(min(Marr))<0
+if min(min(Marr))<1
     error(sprintf('Marr=%d is an unacceptable value',min(Marr)))
 end
 
-switch(nargin)
+switch nargin
     case {1,2,3}
         error('Insufficient parameters passed')
     case {4,5}
         if nargin==4
-            d = zeros(1,xc); % Default to no derivatives being used
+            deriv = zeros(1,xc); % Default to no derivatives being used
         else
-            [dr dc] = size(d);
+            [dr dc] = size(deriv);
             if dc~=xc
                 error(sprintf('dimension mismatch: xc=%d, dc=%d',xc,dc))
+            elseif min(deriv)<0
+                error(sprintf('negative derivative requested, %d',min(deriv)))
             elseif dr~=1
                 error('d must be a row vector of derivatives')
             end
-            if sum(d>0 + d<0)>0 % Right now you can't use derivatives
-                warning(sprintf('%d is an unacceptable derivative, reset to 0',d))
-                d = zeros(1,xc);
+            if sum(deriv>0 + deriv<2)>0 % Right now you only have 1st derivatives
+                warning(sprintf('%d is an unacceptable derivative, reset to 0',deriv))
+                deriv = zeros(1,xc);
             end
         end
         if alpha<=0 || ep<=0 || imag(alpha)~=0 || imag(ep)~=0
@@ -62,19 +70,75 @@ switch(nargin)
         p = zeros(xr,Mc);
         sx2 = sum(x.^2,2);
         for k=1:Mc
-            p(:,k) = rbfphialpha(Marr(:,k),x,ep,alpha,d,beta,delta,sx2);
+            p(:,k) = rbfphialpha(Marr(:,k),x,ep,alpha,deriv,beta,delta,sx2);
         end
     case {6,7}
         error('Too many parameters passed, see comments')
     case 8
-        if sum(d)==0
-        q = -.5*(sum(Marr-1)*log(2)+sum(gammaln(Marr))-xc*log(beta))+delta*sx2;
-        [Hx,Hi] = HermiteProd(Marr-1,beta*alpha*x,logoption);
-        if logoption
-            p = exp(q+Hx).*(-1).^Hi;
+        if sum(deriv)==0 % No derivatives, easier to handle
+            q = -.5*(sum(Marr-1)*log(2)+sum(gammaln(Marr))-xc*log(beta))+delta*sx2;
+            [Hx,Hi] = HermiteProd(Marr-1,beta*alpha*x,logoption);
+            if logoption
+                p = exp(q+Hx).*(-1).^Hi;
+            else
+                p = exp(q).*Hx;
+            end
         else
-            p = exp(q).*Hx;
+            p = ones(xr,1);
+            for k=1:dc
+                m = Marr(k);
+                d = deriv(k);
+                xk = x(:,k);
+                pm = rbfphialpha(m,xk,ep,alpha,0,beta,delta,sx2);
+                switch d
+                    case 0
+                        p = p*pm;
+                    case 1
+                        pm1 = rbfphialpha(m-1,xk,ep,alpha,0,beta,delta,sx2);
+                        p = p*(2*delta*x.*pm+sqrt(2*n-2)*beta*alpha*pm1);
+                    case 2
+                        pm1 = rbfphialpha(m-1,xk,ep,alpha,0,beta,delta,sx2);
+                        pm2 = rbfphialpha(m-2,xk,ep,alpha,0,beta,delta,sx2);
+                        p = p*(2*delta*(1+2*delta*xk.^2).*p + ...
+                               4*delta*beta*alpha*sqrt(2*n-2)*xk.*pm1 + ...
+                               2*(beta*alpha)^2*sqrt((n-1)*(n-2))*pm2;
+                    otherwise
+                        error(sprint('Unacceptable derivative %d in rbfphialpha',d))
+                end
+            end
         end
 end
 
 end
+
+% For Developers only
+% Note: serious improvments may be possible using a recurrence relation -
+%       it will generally be the case that we are interested in computing
+%       all the eigenfucntions phi_m for 1<m<M, in which case we could
+%       exploit the relationship:
+%   p_{n+1}(x)=sqrt(2/n)*beta*alpha*x*p_{n}(x)-sqrt(1-1/n)*p_{n-1}(x)
+%       Doing so would require us to catch the fact that the user has asked
+%       for a sequence of values, and it would also potentially undermine
+%       the log thing, since we'd end up computing alot of the values via
+%       the recurrence.  The ideal setting would be to compute some values
+%       directly and others via the recurrence to balance the savings in
+%       cost with the gains from vectorization.
+%
+%       Of course, along that same thought this whole code could actually
+%       probably be improved by computing more than one column of this
+%       matrix at a time.  Given that, the dominant cost is still the QR
+%       factorization, but it might not always be that way.
+%
+%       Also, I need to fix this whole issue with the indices being off by
+%       one now that we've switched to the alpha evaluation.  I could fix
+%       that in the rbfFormMarr function, but I need to think about this a
+%       little more.
+%
+%       Derivatives could potentially be handled combinatorially to allow
+%       for more efficient computation, but that would be really hard.  The
+%       advantage would be that each term in the summations that represent
+%       the derivatives could be handled with logs.
+%       Also for derivatives, when we switch to the asymptotic Hermite's
+%       there may be a safer way to compute the derivative.  Since the
+%       Hermite's have an asymptotic form, that could be differentiated
+%       directly rather than using the recurrence relation.
