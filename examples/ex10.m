ba7975d69e0aec34b601b067834176d744b23ded
Michael McCourt <mccomic@mcs.anl.gov>
2011-09-23 18:11:10 -0500

    Added rbfalphasearch to help find a good integral.  It still needs work.

--- examples/ex10.m
+++ examples/ex10.m
@@ -3,25 +3,26 @@
 rbfsetup
 weightfunc = @(a,x) a/sqrt(pi)*exp(-a^2*x.^2);
 epvec = [.1 1 10]; % Shape parameter
-ptopt = 'even'; % Point distribution to consider
-index = 10; % Eigenfunction orthogonality to test
+indexvec = [1 5 10]; % Eigenfunction orthogonality to test
 Na = 100; % Number of alphas to look at
 a = -3;
 b = 3;
 
 k=1;
-alphavec = logspace(-2,2,Na);
-for ep=epvec
-    erra = zeros(1,Na);
-    j = 1;
-    for alpha=alphavec
-        erra(j) = quadl(@(x)rbfphialpha(index,x',ep,alpha)'.*weightfunc(alpha,x).*rbfphialpha(index,x',ep,alpha)',a,b);
-        j = j+1;
+alphavec = logspace(-2,1,Na);
+for index=indexvec
+    for ep=epvec
+        erra = zeros(1,Na);
+        j = 1;
+        for alpha=alphavec
+            erra(j) = quadl(@(x)rbfphialpha(index,x',ep,alpha)'.*weightfunc(alpha,x).*rbfphialpha(index,x',ep,alpha)',a,b);
+            j = j+1;
+        end
+        subplot(length(indexvec),length(epvec),k)
+        semilogx(alphavec,erra)
+        xlabel('\alpha')
+        ylabel('integral appx')
+        title(sprintf('ep=%g, index=%d',ep,index))
+        k = k+1;
     end
-    subplot(length(epvec),1,k)
-    semilogx(alphavec,erra)
-    xlabel('\alpha')
-    ylabel('integral appx')
-    title(sprintf('ep=%g, index=%d',ep,index))
-    k = k+1;
 end
---
+++ source/rbfalphasearch.m
@@ -0,0 +1,48 @@
+function [alpha,k] = rbfalphasearch(ep,a,b)
+% function alpha = rbfalphasearch(ep,a,b)
+% ep - shape parameter for the Gaussians
+% a - minimum bound(s) of domain
+% b - maximum bound(s) of domain
+%   In multiple dimensions these should be a=[low_1 ... low_d]
+%                                          b=[high_1 ... high_d]
+%
+% This function tries to return the smallest alpha for which orthonormality
+% is maintained throughout the domain until a set eigenfunction.
+%
+% You can set the top eigenfunction for which orthonornmality should be
+% maintained in rbfsetup with the value
+%      GAUSSQR_PARAMETERS.DEFAULT_ORTH_REQUESTED
+%
+% If this function cannot find an alpha up to what was requested, it will
+% try to get as high as possible.  The index up to which orthogonality was
+% maintained is returned as k.
+
+% Import global parameters from rbfsetup
+global GAUSSQR_PARAMETERS
+if ~isstruct(GAUSSQR_PARAMETERS)
+    error('GAUSSQR_PARAMETERS does not exist ... did you forget to call rbfsetup?')
+end
+orthmax = GAUSSQR_PARAMETERS.DEFAULT_ORTH_REQUESTED;
+
+k = orthmax;
+alpha = fminbnd(@(alpha)-rbforthintegral(k,ep,alpha,a,b),1e-3,1e3);
+
+end
+
+function integral = rbforthintegral(k,ep,alpha,a,b)
+% Import global parameters from rbfsetup
+global GAUSSQR_PARAMETERS
+if ~isstruct(GAUSSQR_PARAMETERS)
+    error('GAUSSQR_PARAMETERS does not exist ... did you forget to call rbfsetup?')
+end
+tol = GAUSSQR_PARAMETERS.DEFAULT_ORTH_TOLERANCE;
+if exist('quadgk')
+    quadgkEXISTS = true;
+end
+
+weight = @(a,x) a/sqrt(pi)*exp(-(a*x).^2);
+
+intappx = quadl(@(x)rbfphialpha(k,x',ep,alpha)'.^2.*weight(alpha,x),a,b);
+integral = (abs(1-intappx)<tol)*(1/alpha);
+
+end
