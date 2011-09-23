529f9408aac926e3087d3943548727f889badce9
Michael McCourt <mccomic@mcs.anl.gov>
2011-09-23 16:35:53 -0500

    Change to orthogonality test

--- examples/ex10.m
+++ examples/ex10.m
@@ -3,28 +3,25 @@
 rbfsetup
 weightfunc = @(a,x) a/sqrt(pi)*exp(-a^2*x.^2);
 epvec = [.1 1 10]; % Shape parameter
-Nvec = [30 100 300]; % Number of points for the trapezoid rule
 ptopt = 'even'; % Point distribution to consider
-index = 1; % Eigenfunction orthogonality to test
+index = 10; % Eigenfunction orthogonality to test
 Na = 100; % Number of alphas to look at
+a = -3;
+b = 3;
 
 k=1;
-for N=Nvec
-    xx = pickpoints(-3,3,N,ptopt);
-    alphavec = logspace(-2,2,Na);
-    for ep=epvec
-        erra = zeros(1,Na);
-        j = 1;
-        for alpha=alphavec
-            phi_j = rbfphialpha(index,xx,ep,alpha);
-            erra(j)=trapz(xx,phi_j.*weightfunc(alpha,xx).*phi_j);
-            j = j+1;
-        end
-        subplot(length(epvec),length(Nvec),k)
-        semilogx(alphavec,erra)
-        xlabel('\alpha')
-        ylabel('integral appx')
-        title(sprintf('N=%d, ep=%g, index=%d',N,ep,index))
-        k = k+1;
+alphavec = logspace(-2,2,Na);
+for ep=epvec
+    erra = zeros(1,Na);
+    j = 1;
+    for alpha=alphavec
+        erra(j) = quadl(@(x)rbfphialpha(index,x',ep,alpha)'.*weightfunc(alpha,x).*rbfphialpha(index,x',ep,alpha)',a,b);
+        j = j+1;
     end
+    subplot(length(epvec),1,k)
+    semilogx(alphavec,erra)
+    xlabel('\alpha')
+    ylabel('integral appx')
+    title(sprintf('ep=%g, index=%d',ep,index))
+    k = k+1;
 end
