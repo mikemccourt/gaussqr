310b91ca51300c414bfdd6f2e4af24398253622e
Michael McCourt <mccomic@mcs.anl.gov>
2012-03-30 01:58:11 -0500

    Cleaned it up a little more Except for b, it looks optimal

--- source/computeQReig.m
+++ source/computeQReig.m
@@ -22,22 +22,27 @@ function [invU,Svec,Q] = computeQReig(M,x,ep,alpha)
 % factorization via Householder or whatever.  I still have some more
 % stability analysis to do, but you should be aware that for larger M it
 % could be an issue.
+%
+% NOTE: TM should have a 1/(beta*alpha) term in it, but that is applied
+% analytically throughout because TM always appears with a beta*alpha.  It
+% seemed unnecessary to apply it and then immediately undo it.
+%
+% Possible optimization may exist for computing b.  In the Hermite paper,
+% it was computed without an inner product, but I haven't been able to
+% determine how that was done, so I haven't been able to make it happen
+% here.  Will look at that soon.
 
 N = size(x,1);
 invU = zeros(M);
 Svec = zeros(M,1);
-Qvec = zeros(M,1);
 Q = zeros(N,M);
 
-beta = (1+(2*ep/alpha)^2)^.25;
-ba = beta*alpha;
-delta2 = .5*alpha^2*(beta^2-1);
+Dx = (1+(2*ep/alpha)^2)^.25*alpha*x;
 
 phi = rbfphi(1:M,x,ep,alpha);
-TM = 1/(sqrt(2)*ba)*(diag(sqrt(1:M-1),-1)+diag(sqrt(1:M-1),1));
 
 tmp = phi'*phi(:,[1,M-1:M]);
-d = tmp(:,2)*sqrt(M-1)/(sqrt(2)*ba) - ApplyTM(tmp(:,3),ba);
+d = tmp(:,2)*sqrt(M-1)/sqrt(2) - ApplyTM(tmp(:,3));
 
 % Start with the first columnns
 u0 = eye(M,1);
@@ -46,37 +51,35 @@ v0 = tmp(:,1);
 Svec(1) = sqrt(u0'*v0);
 invU(:,1) = u0;
 
-q0 = exp(-delta2*x.^2);
-q0 = q0/norm(q0);
+q0 = phi(:,1)/Svec(1);
 Q(:,1) = q0;
 
 % Get the second columns
-
-tmp = ApplyTM(u0,ba);
-c = -ba*tmp'*v0/Svec(1)^2;
-u1 = sqrt(2)*(ba*tmp+c*u0);
-v1 = sqrt(2)*(ba*ApplyTM(v0,ba) + c*v0);
-v1(M) = v1(M) - sqrt(2)*ba*(d'*u0);
+tmp = ApplyTM(u0);
+c = -tmp'*v0/Svec(1)^2;
+u1 = sqrt(2)*(tmp+c*u0);
+v1 = sqrt(2)*(ApplyTM(v0) + c*v0);
+v1(M) = v1(M) - sqrt(2)*(d'*u0);
 
 Svec(2) = sqrt(u1'*v1);
 invU(:,2) = u1;
 
-q1 = sqrt(2)*(ba*x.*q0 + c*q0)*Svec(1)/Svec(2);
+q1 = (sqrt(2)*Svec(1)/Svec(2))*(Dx.*q0 + c*q0);
 Q(:,2) = q1;
 
+% Iterate through the remaining columns
 for k = 2:M-1
-
-    tmp = ApplyTM(u1,ba);
-    c = -ba/Svec(k)^2*(v1'*tmp);
-    b = sqrt((k-1)/2)*ba/Svec(k-1)^2*(v0'*tmp);
-    u2 = sqrt(2/k)*(ba*tmp+c*u1) - 2*b/sqrt(k^2-k)*u0;
-    v2 = sqrt(2/k)*(ba*ApplyTM(v1,ba)+c*v1) - 2*b/sqrt(k^2-k)*v0;
-    v2(M) = v2(M) - sqrt(2/k)*ba*(d'*u1);
+    tmp = ApplyTM(u1);
+    c = -(v1'*tmp)/Svec(k)^2;
+    b = sqrt((k-1)/2)/Svec(k-1)^2*(v0'*tmp);
+    u2 = sqrt(2/k)*(tmp+c*u1) - (2*b/sqrt(k^2-k))*u0;
+    v2 = sqrt(2/k)*(ApplyTM(v1)+c*v1) - (2*b/sqrt(k^2-k))*v0;
+    v2(M) = v2(M) - sqrt(2/k)*(d'*u1);
     
     Svec(k+1) = sqrt(u2'*v2);
     invU(:,k+1) = u2;
     
-    q2 = sqrt(2/k)*(ba*x.*q1+c*q1)*(Svec(k)/Svec(k+1)) - (2*b/sqrt(k^2-k))*(Svec(k-1)/Svec(k+1))*q0;
+    q2 = (sqrt(2/k)*Svec(k)/Svec(k+1))*(Dx.*q1+c*q1) - (2*b/sqrt(k^2-k)*Svec(k-1)/Svec(k+1))*q0;
     Q(:,k+1) = q2;
 
     u0 = u1; u1 = u2;
@@ -87,20 +90,22 @@ end
 
 % This is a private function that shouldn't be accessed outside this file
 % It applies the matrix
-%      TM = (diag(sqrt(1:M-1),1) + diag(sqrt(1:M-1),-1))/(sqrt(2)*beta*alpha)
+%      TM = (diag(sqrt(1:M-1),1) + diag(sqrt(1:M-1),-1))/sqrt(2)
 % to a vector without forming that matrix
 % M is the length of the vector
 %
-% NOTE: this function could avoid being passed ba, but I need to make some
-% changes throughout to make that viable
+% NOTE: this function avoids being passed ba by applying it analytically
+% throughout above.  Therefore, this is NOT the same TM matrix that appears
+% in the paper.
 %
-% Also, it only should be passed column vectors, not multiple columns yet
-function TMx = ApplyTM(x,ba)
+% Also, it only should be passed column vectors, not multiple columns.
+% This shouldn't be a problem, but I'm leaving a note in case I go senile.
+function TMx = ApplyTM(x)
 persistent M Mvec k;
 if isempty(Mvec)
     M = size(x,1);
     k = 1;
-    Mvec = sqrt(1:M-1)'/(sqrt(2)*ba);
+    Mvec = sqrt(1:M-1)'/sqrt(2);
 end
 TMx = ([Mvec.*x(2:end);0] + [0;Mvec.*x(1:end-1)]);
 end
