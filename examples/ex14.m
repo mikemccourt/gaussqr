2abcbace42f7247e98a78cc814f65a318fdc9cc4
Michael McCourt <mccomic@mcs.anl.gov>
2012-04-07 09:51:42 -0500

    Changes to ex14 to use fast phi evaluation

--- examples/ex14.m
+++ examples/ex14.m
@@ -1,10 +1,17 @@
 % ex14
 % This tests the speed of the fast QR factorization in 1D.
+% Note that it takes advantage of the fast phi evaluation, so as to not
+% bias the cost of the QR solve
+global GAUSSQR_PARAMETERS
+if ~isstruct(GAUSSQR_PARAMETERS)
+    error('GAUSSQR_PARAMETERS does not exist ... did you forget to call rbfsetup?')
+end
+GAUSSQR_PARAMETERS.FAST_PHI_EVALUATION = 1;
 
 % These are the values I'm interested in testing
 % Note M is stored as a percentage of N
-Nvec = 50*2.^(0:7);
-Mvec = .1*(1:8);
+Nvec = 100*2.^(0:7);
+Mvec = .01*2.^(0:5);
 
 % These values are arbitrary, because I don't really care about the
 % solution, just the time it takes to find it
@@ -12,6 +19,10 @@ ep = 1;
 alpha = 1;
 
 timeMat = zeros(length(Nvec),length(Mvec));
+slowMat = zeros(length(Nvec),length(Mvec));
+
+% Ignore because we're not concerned about the solution accuracy
+warning off
 
 n = 1;
 for N=Nvec
@@ -24,9 +35,16 @@ for N=Nvec
         tic
         c = computeQReig(M,x,ep,alpha,b);
         timeMat(n,m) = toc;
-        fprintf('%d\t',M)
+        tic
+        phi = rbfphi(1:M,x,ep,alpha);
+        c = phi\b;
+        slowMat(n,m) = toc;
+        fprintf('%d\t%d\t%g\t%g\n',M,N,timeMat(n,m),slowMat(n,m))
         m = m + 1;
     end
-    fprintf('  |  %d\n',N)
     n = n + 1;
 end
+
+warning on
+
+save('speedtest.mat','slowMat','timeMat')
