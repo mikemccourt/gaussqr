0618049fc631444a7f265a8fb8fd4080a88b6051
Michael McCourt <mccomic@mcs.anl.gov>
2012-04-12 18:05:55 -0500

    Changed this to more accurately represent fast QR picture

--- examples/ex14.m
+++ examples/ex14.m
@@ -10,54 +10,18 @@ GAUSSQR_PARAMETERS.FAST_PHI_EVALUATION = 1;
 
 % These are the values I'm interested in testing
 % Note M is stored as a percentage of N
-Nvec = 1000*2.^(0:8);
-Mvec = .001*2.^(0:5);
+Nvec = 200*2.^(0:7);
+Mvec = .01*2.^(0:5);
 
 % These values are arbitrary, because I don't really care about the
 % solution, just the time it takes to find it
 ep = 1;
 alpha = 1;
 
-fastMat = zeros(length(Nvec),length(Mvec));
-slowMat = zeros(length(Nvec),length(Mvec));
-fastFull = zeros(length(Nvec),length(Mvec));
-slowFull = zeros(length(Nvec),length(Mvec));
-
-% Ignore because we're not concerned about the solution accuracy
-warning off
-
-n = 1;
-for N=Nvec
-    b = rand(N,1);
-    x = pickpoints(0,1,N);
-    m = 1;
-    for Mp=Mvec
-        M = floor(Mp*N);
-        c = zeros(M,1);
-        tic
-%         c = computeQReig_adjusted(M,x,ep,alpha,b);
-        c = computeQReig(M,x,ep,alpha,b);
-        fastMat(n,m) = toc;
-        tic
-        phi = rbfphi(1:M,x,ep,alpha);
-        c = phi\b;
-        slowMat(n,m) = toc;
-        tic
-%         [A,B,C] = computeQReig_adjusted(M,x,ep,alpha);
-        [A,B,C] = computeQReig(M,x,ep,alpha);
-        fastFull(n,m) = toc;
-        tic
-        phi = rbfphi(1:M,x,ep,alpha);
-        [Q,R] = qr(phi,0);
-        slowFull(n,m) = toc;
-        fprintf('%d\t%d\t%g\t%g\t%g\t%g\n',M,N,fastMat(n,m),slowMat(n,m),fastFull(n,m),slowFull(n,m))
-        m = m + 1;
-    end
-    n = n + 1;
-end
-
 M = 1024;
 Nvec = [3200,6400,12800,25600,51200];
+
+warning off
 
 for N=Nvec
     b = rand(N,1);
@@ -70,9 +34,30 @@ for N=Nvec
     phi = rbfphi(1:M,x,ep,alpha);
     c = phi\b;
     slowMN = toc;
-    [timeMN,slowMN]
+    fprintf('%d\t%d\t%g\t%g\n',M,N,timeMN,slowMN)
+end
+
+fprintf('\n\n')
+
+N = 50000;
+Mvec = [4,8,16,32,64,128,256,512,1024];
+b = rand(N,1);
+
+for M=Mvec
+    r = rbfroots(M,ep,alpha);
+    x = pickpoints(-r,r,N);
+    c = zeros(M,1);
+    tic
+    c = computeQReig(M,x,ep,alpha,b);
+    timeMN = toc;
+    tic
+    phi = rbfphi(1:M,x,ep,alpha);
+    c = phi\b;
+    slowMN = toc;
+%    rank(phi) % just so that I know
+    fprintf('%d\t%d\t%g\t%g\n',M,N,timeMN,slowMN)
 end
 
 warning on
 
-save('speedtest.mat','slowMat','fastMat','slowFull','fastFull')
+% save('speedtest.mat','slowMat','timeMat')
