da9b6b2339d8346c12cbe8dcf4d18a7ec1e1724d
Michael McCourt <mccomic@mcs.anl.gov>
2012-04-29 20:48:30 -0500

    Added mqr_solveprep to allow for MaternQR work Need to consider possible series length alternatives

---
+++ source/maternqr/mqr_solveprep.m
@@ -0,0 +1,61 @@
+function [L,ep,beta,Mmax] = mqr_solveprep(x,L,ep,beta)
+% function [L,ep,beta,Mmax] = mqr_solveprep(x,L,ep,beta)
+%
+% This function takes all the stuff needed to do a GaussQR problem and
+% preps it before the problem starts.  The reason for this is so that I can
+% get some meta-info without calling the whole solve routine.  Also,
+% changes which are the same for both QR and QRr will only need changed at
+% one place rather than multiple files
+%
+% INPUTS: x - input data values
+%         L - boundary of the domain
+%         ep - value of shape parameter
+%         beta - <default=1> smoothness parameter
+% OUTPUTS: L - boundary of the domain
+%          ep - value of shape parameter
+%          beta - smoothness parameter
+%          Mmax - length of the necessary eigenfunction expansion
+global GAUSSQR_PARAMETERS
+if ~isstruct(GAUSSQR_PARAMETERS)
+    error('GAUSSQR_PARAMETERS does not exist ... did you forget to call rbfsetup?')
+end
+Mextramax = GAUSSQR_PARAMETERS.MAX_EXTRA_EFUNC;
+
+[N,d] = size(x);
+
+% Checks to make sure that the ep and beta values are acceptable
+if length(ep)>1
+    ep = abs(real(ep(1)));
+    warning('Multiple epsilon values not allowed; using epsilon=%g',ep)
+end
+if abs(real(ep))~=ep
+    ep = abs(real(ep));
+    warning('Only real, positive epsilon allowed; using epsilon=%g',ep)
+end
+if length(beta)>1
+    beta = abs(real(beta(1)));
+    warning('Multiple beta values not allowed; using beta=%g',beta)
+end
+if beta~=floor(abs(real(beta)))
+    beta = floor(abs(real(beta)));
+    warning('Non-integer beta values not yet allowed; using beta=%d',beta)
+end
+
+% Checks to make sure that L is an acceptable value
+if x~=abs(real(x))
+    error('x must have nonnegative, real values only')
+end
+maxx = max(x);
+if L<=maxx
+    L = maxx
+    warning('Data passed beyond [0,L] domain, resetting L=%g',L)
+end
+
+% This picks the eigenfunction series length that we want to use for the
+% mqr_solve.  The minimum length is 1.01*N, although eventually I'll allow
+% for smaller M values
+if Mextramax<0
+    Mmax = ceil((1-(min(Mextramax,-101)/100))*N);
+else
+    Mmax = ceil(max(Mextramax,1.01*N));
+end
