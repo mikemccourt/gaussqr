9782d074e03a14ac977b9691500d997e6c55ded4
Michael McCourt <mccomic@mcs.anl.gov>
2012-04-29 19:43:49 -0500

    Added derivatives for the ep=0 case

--- source/maternqr/cmatern.m
+++ source/maternqr/cmatern.m
@@ -123,10 +123,10 @@ end
 % For certain values we have the closed form of the function
 % Otherwise we need to compute the series
 if ep==0 && Mfix==0
-    bp = BernoulliPoly(2*beta,.5*(x+z)/L);
-    s1 = (BernoulliPoly(2*beta,.5*(x-z)/L) - bp).*(x>=z);
-    s2 = (BernoulliPoly(2*beta,.5*(z-x)/L) - bp).*(x<z);
-    s = exp((2*beta-1)*log(2*L)-gammaln(2*beta+1))*(s1+s2);
+    bp = BernoulliPoly(2*beta-deriv,(x+z)/(2*L));
+    s1 = (BernoulliPoly(2*beta-deriv,(x-z)/(2*L)) - bp).*(x>=z);
+    s2 = ((-1)^deriv*BernoulliPoly(2*beta-deriv,(z-x)/(2*L)) - bp).*(x<z);
+    s = (-1)^(beta+1)*exp((2*beta-1-deriv)*log(2*L)-gammaln(2*beta+1-deriv))*(s1+s2);
 elseif beta==1 && Mfix==0
     s = sinh(ep*min(x,z)).*sinh(ep*(L-max(x,z)))./(ep*sinh(L*ep));
 else
