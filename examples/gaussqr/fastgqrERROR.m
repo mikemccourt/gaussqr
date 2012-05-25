164d22c8d3a05f5605e70249a50cbe7e05105055
Michael McCourt <mccomic@mcs.anl.gov>
2012-05-25 14:56:41 -0500

    Added a test for the fast QR error, not done yet

---
+++ examples/gaussqr/fastgqrERROR.m
@@ -0,0 +1,62 @@
+% Look at the error when using various N and M combinations
+f = @(x) besselj(0,x);
+
+N = 200;
+NN = 1000;
+x = pickpoints(0,1,N);
+xx = pickpoints(0,1,NN);
+y = f(x);
+yy = f(xx);
+
+Mvec = 5:5:100;
+ep = .001;
+alpha = 1;
+opts.UT = true;
+Mtol = 1e-13;
+
+errorth_true = zeros(size(Mvec));
+errorth_fast = zeros(size(Mvec));
+errorth_fnew = zeros(size(Mvec));
+
+errorth_Strue = zeros(size(Mvec));
+errorth_Sfast = zeros(size(Mvec));
+errorth_Sfnew = zeros(size(Mvec));
+m = 1;
+for M=Mvec
+    I = eye(M);
+    GQR = gqr_rsolve(x,y,ep,alpha,M);
+    
+    Marr = gqr_formMarr(M);
+    phi = gqr_phi(Marr,x,ep,alpha);
+    [Q,R] = qr(phi,0);
+    errorth_true(m) = norm(Q'*Q - I);
+    GQR.coef = R\(Q'*y);
+    [yp,GQR] = gqr_eval(GQR,xx);
+    errorth_Strue(m) = errcompute(yp,yy);
+    
+    [invU,Svec,Q] = computeQReig(M,x,ep,alpha);
+    errorth_fast(m) = norm(Q'*Q - I);
+    t = Q'*y;
+    Mcut = min(find(abs(t)<Mtol));
+    if isempty(Mcut)
+        Mcut = M;
+    end
+    t = [t(1:Mcut);zeros(M-Mcut,1)];
+    GQR.coef = invU*((1./Svec).*t);
+    yp = gqr_eval(GQR,xx);
+    errorth_Sfast(m) = errcompute(yp,yy);
+    
+    [invU,Svec,Q] = computeQReig_adjusted(M,x,ep,alpha);
+    errorth_fnew(m) = norm(Q'*Q - I);
+    t = Q'*y;
+    Mcut = min(find(abs(t)<Mtol));
+    if isempty(Mcut)
+        Mcut = M;
+    end
+    t = [t(1:Mcut);zeros(M-Mcut,1)];
+    GQR.coef = invU*((1./Svec).*t);
+    yp = gqr_eval(GQR,xx);
+    errorth_Sfnew(m) = errcompute(yp,yy);
+    
+    m = m+1;
+end
