c7cf590e9a334c170608dd71cc208a12e9a3d42e
wmayner <wmayner@math.iit.edu>
2012-07-12 14:45:13 -0500

    Fixing kernel expression....

--- examples/maternqr/testfunctions/testkernel.m
+++ examples/maternqr/testfunctions/testkernel.m
@@ -5,11 +5,21 @@ function out = testkernel(x)
 
 % set the plane with which to slice our 2-D kernel to obtain a 1-D curve:
 y = .5; % this is where the discontinuity in the 3rd derivative will occur
+eps = 1;
+L = 1;
 
 out = zeros(length(x),1);
 for i = 1:length(x)
     if (0 <= x(i) )&& (x(i) < y)
-        out(i) = (1/(2*(exp(2)-1))^2)*((exp(2)-1)*x(i)*cosh(x(i))*((exp(2)-1)*cosh(y)-(1+exp(2))*sinh(y))+sinh(x(i))*(cosh(y)-sinh(y))*(exp(2)*(y-1)-exp(4)*(y+1)+(y-1)*(sinh(2*y)+cosh(2*y))-(y-3)*sinh(2*(y+1))+cosh(2*(y+1))));
+        out(i) = 
+        
+        (4*eps*x(i)*sinh(eps*L)*cosh(eps*x(i))*(sinh(2*eps*L)+cosh(2*eps*L))*sinh(eps*(L-y))) + ...
+        sinh(eps*x(i))*(cosh(eps*y)-sinh(eps*y))*((-2*eps*L+eps*y+1)*(sinh(2*eps*L)+cosh(2*eps*L)) - ...
+            (eps*y+1)*(sinh(4*eps*L)+cosh(4*eps*L)) + ...
+            (2*eps*L-eps*y+1)*(sinh(2*eps*(L+y)) + cosh(2*eps*(L+y))) + ...
+            (eps*y-1)*(sinh(2*eps*y)+cosh(2*eps*y)));
+        
+        (1/(2*(exp(2)-1))^2)*((exp(2)-1)*x(i)*cosh(x(i))*((exp(2)-1)*cosh(y)-(1+exp(2))*sinh(y))+sinh(x(i))*(cosh(y)-sinh(y))*(exp(2)*(y-1)-exp(4)*(y+1)+(y-1)*(sinh(2*y)+cosh(2*y))-(y-3)*sinh(2*(y+1))+cosh(2*(y+1))));
     elseif ((y <= x(i)) && (x(i) <= 1))
         out(i) = (1/(2*(exp(2)-1))^2)*((exp(2)-1)*y*cosh(y)*((exp(2)-1)*cosh(x(i))-(1+exp(2))*sinh(x(i)))+sinh(y)*(cosh(x(i))-sinh(x(i)))*(exp(2)*(x(i)-1)-exp(4)*(x(i)+1)+(x(i)-1)*(sinh(2*x(i))+cosh(2*x(i)))-(x(i)-3)*sinh(2*(x(i)+1))+cosh(2*(x(i)+1))));
     else
