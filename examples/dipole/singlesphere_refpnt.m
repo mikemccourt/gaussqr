cb477d0692ed10f6ca6ef8dfb806916b1edc6c74
Salvatore Ganci <salvatore.ganci@unipa.it>
2013-02-05 03:14:56 -0600

    Fixed a silly mistake.

--- examples/dipole/singlesphere_refpnt.m
+++ examples/dipole/singlesphere_refpnt.m
@@ -5,13 +5,13 @@
 %  The script generates a plot showing how the error on the solution
 %  behaves when we move the reference point over the sphere.
 %
-%  We consider the solution to the Laplace equation on a sphere with: 
+%  We consider the solution to the Laplace equation on a sphere with:
 %  1) Neumann boundary conditions only, by evaluating potential differences
 %     with respect to a reference point;
 %  2) Neumann boundary conditions and one Dirichlet condition at the
 %     reference point (zero potential point).
 %
-%  The problem has several physical parameters relating to the underlying 
+%  The problem has several physical parameters relating to the underlying
 %  EEG/MEG physical system.  These parameters are:
 %    R - Sphere radius [dm] <default = 1>
 %    sig - Electric conductivity [S/dm] <default = .02>
@@ -33,11 +33,11 @@
 %     mfs_sphere - Fraction beyond R (eg, 1.3R) for centers <default = 1.3>
 %     BC_choice -  How to choose the boundary conditions <default = 1>
 %                 1 : Neumann
-%                 2 : Neumann + one Dirichlet at a reference point (zero 
+%                 2 : Neumann + one Dirichlet at a reference point (zero
 %                     potential point)
 %
 %  Other parameters to be set:
-%     Npnts - Number of desired collocation points <default = 600>
+%     Npnts - Number of desired collocation points <default = 800>
 %     dip_cushion - How much space should be given around the dipole where
 %                   no RBF centers are allowed <default = .005>
 %     N_eval - # evaluation points <default = 500>
@@ -61,9 +61,9 @@ radbasfun = 'imq';
 ep = 1;
 mfs_frac = 1.0;
 mfs_sphere = 1.3;
-BC_choice = 1;
+BC_choice = 2;
 
-Npnts = 600;
+Npnts = 800;
 dip_cushion = .005; % Not yet implemented
 N_eval = 500;
 
@@ -108,55 +108,45 @@ N_int = size(intdata,1);
 N_bdy = size(bdydata,1);
 N_tot = N_int + N_bdy;
 
-% Compose a vector of all the RBF centers
-% In the MFS setting, these are chosen in a sphere around the ball
-if strcmp(sol_type,'mfs')
-    ctrs = SphereSurfGoldPoints(floor(mfs_frac*Npnts), mfs_sphere*R);
-else % For kansa, the centers and collocation points coincide
-    if BC_choice == 1
-        ctrs = [intdata; bdydata];
-    else % In this case we need an "extra" center (reference point)
-        ctrs = [intdata; bdydata; refpnt];
-        N_tot = N_tot + 1;
-    end
-end
-
-% Compute the collocation block for the interior
-DM_intdata = DistanceMatrix(intdata,ctrs);
-LCM = Lrbf(ep,DM_intdata);
-rhs_int = zeros(N_int,1);
-
-% Compute the evaluation matrix
-DM_eval = DistanceMatrix(evalpnts, ctrs);
-EM = rbf(ep, DM_eval);
-
-% Determine which points are Neumann
-    bdydata_neu = bdydata;
-    normvecs = NORMALS.n11;
-
-% Compute the collocation block for the boundary conditions
-% This also computes the RHS for the problem
-% First we consider the Neumann BC component
-DM_bdydata_neu = DistanceMatrix(bdydata_neu,ctrs);
-
-% Find all the necessary difference matrices
-dx_bdydata_neu = DifferenceMatrix(bdydata_neu(:,1),ctrs(:,1));
-dy_bdydata_neu = DifferenceMatrix(bdydata_neu(:,2),ctrs(:,2));
-dz_bdydata_neu = DifferenceMatrix(bdydata_neu(:,3),ctrs(:,3));
-
-% Compute normal derivative collocation matrix for boundary
-A = repmat(normvecs(:,1),1,N_tot).*dxrbf(ep,DM_bdydata_neu,dx_bdydata_neu);
-B = repmat(normvecs(:,2),1,N_tot).*dyrbf(ep,DM_bdydata_neu,dy_bdydata_neu);
-C = repmat(normvecs(:,3),1,N_tot).*dzrbf(ep,DM_bdydata_neu,dz_bdydata_neu);
-BCM_neu = A + B + C;
-
-% Compute known-terms vector (a.k.a. righthand side vector)
-% This requires the gradient of the unbounded potential at boundary
-gradphi_F_neu = gradphiF_dip(bdydata_neu, srcpnts, dipmom, sig);
-rhs_bdy_neu = -sum(normvecs.*gradphi_F_neu,2);
-
 errvec = zeros(N_eval,1);
 if BC_choice == 1
+    % Compose a vector of all the RBF centers
+    ctrs = [intdata; bdydata];
+    
+    % Compute the collocation block for the interior
+    DM_intdata = DistanceMatrix(intdata,ctrs);
+    LCM = Lrbf(ep,DM_intdata);
+    rhs_int = zeros(N_int,1);
+    
+    % Compute the evaluation matrix
+    DM_eval = DistanceMatrix(evalpnts, ctrs);
+    EM = rbf(ep, DM_eval);
+    
+    % Determine which points are Neumann
+    bdydata_neu = bdydata;
+    normvecs = NORMALS.n11;
+    
+    % Compute the collocation block for the boundary conditions
+    % This also computes the RHS for the problem
+    % First we consider the Neumann BC component
+    DM_bdydata_neu = DistanceMatrix(bdydata_neu,ctrs);
+    
+    % Find all the necessary difference matrices
+    dx_bdydata_neu = DifferenceMatrix(bdydata_neu(:,1),ctrs(:,1));
+    dy_bdydata_neu = DifferenceMatrix(bdydata_neu(:,2),ctrs(:,2));
+    dz_bdydata_neu = DifferenceMatrix(bdydata_neu(:,3),ctrs(:,3));
+    
+    % Compute normal derivative collocation matrix for boundary
+    A = repmat(normvecs(:,1),1,N_tot).*dxrbf(ep,DM_bdydata_neu,dx_bdydata_neu);
+    B = repmat(normvecs(:,2),1,N_tot).*dyrbf(ep,DM_bdydata_neu,dy_bdydata_neu);
+    C = repmat(normvecs(:,3),1,N_tot).*dzrbf(ep,DM_bdydata_neu,dz_bdydata_neu);
+    BCM_neu = A + B + C;
+    
+    % Compute known-terms vector (a.k.a. righthand side vector)
+    % This requires the gradient of the unbounded potential at boundary
+    gradphi_F_neu = gradphiF_dip(bdydata_neu, srcpnts, dipmom, sig);
+    rhs_bdy_neu = -sum(normvecs.*gradphi_F_neu,2);
+    
     % Create the full linear system from the blocksand solve it
     % Compose rhs
     rhs = [rhs_int;rhs_bdy_neu];
@@ -177,8 +167,48 @@ if BC_choice == 1
         errvec(i) = errcompute(phi_comp,phi_true);
     end
 else
+    N_tot = N_tot + 1; % In this case we need an "extra" center
+    % (reference point)
     for i = 1:N_eval
         fprintf('iter %d of %d\n',i,N_eval);
+        
+        % Compose a vector of all the RBF centers
+        ctrs = [intdata; bdydata; evalpnts(i,:)];
+        
+        % Compute the collocation block for the interior
+        DM_intdata = DistanceMatrix(intdata,ctrs);
+        LCM = Lrbf(ep,DM_intdata);
+        rhs_int = zeros(N_int,1);
+        
+        % Compute the evaluation matrix
+        DM_eval = DistanceMatrix(evalpnts, ctrs);
+        EM = rbf(ep, DM_eval);
+        
+        % Determine which points are Neumann
+        bdydata_neu = bdydata;
+        normvecs = NORMALS.n11;
+        
+        % Compute the collocation block for the boundary conditions
+        % This also computes the RHS for the problem
+        % First we consider the Neumann BC component
+        DM_bdydata_neu = DistanceMatrix(bdydata_neu,ctrs);
+        
+        % Find all the necessary difference matrices
+        dx_bdydata_neu = DifferenceMatrix(bdydata_neu(:,1),ctrs(:,1));
+        dy_bdydata_neu = DifferenceMatrix(bdydata_neu(:,2),ctrs(:,2));
+        dz_bdydata_neu = DifferenceMatrix(bdydata_neu(:,3),ctrs(:,3));
+        
+        % Compute normal derivative collocation matrix for boundary
+        A = repmat(normvecs(:,1),1,N_tot).*dxrbf(ep,DM_bdydata_neu,dx_bdydata_neu);
+        B = repmat(normvecs(:,2),1,N_tot).*dyrbf(ep,DM_bdydata_neu,dy_bdydata_neu);
+        C = repmat(normvecs(:,3),1,N_tot).*dzrbf(ep,DM_bdydata_neu,dz_bdydata_neu);
+        BCM_neu = A + B + C;
+        
+        % Compute known-terms vector (a.k.a. righthand side vector)
+        % This requires the gradient of the unbounded potential at boundary
+        gradphi_F_neu = gradphiF_dip(bdydata_neu, srcpnts, dipmom, sig);
+        rhs_bdy_neu = -sum(normvecs.*gradphi_F_neu,2);
+        
         % Consider the Dirichlet condition
         bdydata_dir = evalpnts(i,:);
         DM_bdydata_dir = DistanceMatrix(bdydata_dir,ctrs);
@@ -199,7 +229,7 @@ else
         phi_comp = phi0 + phi_F;
         
         phi_true = phi_an - phi_an(i);
-        errvec(i) = errcompute(phi_comp,phi_true);        
+        errvec(i) = errcompute(phi_comp,phi_true);
     end
 end
 
