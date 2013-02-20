55131769d52341b473ee901167cc9b7a6963950d
Salvatore Ganci <salvatore.ganci@unipa.it>
2013-02-20 12:55:54 -0600

    Allowed different BCs also for the current density problem

--- examples/dipole/singlesphere_N_current.m
+++ examples/dipole/singlesphere_N_current.m
@@ -28,6 +28,16 @@
 %     ep - RBF shape parameter <default = 10>
 %     mfs_frac - How many centers for MFS, in [0.0,1.0]*N <default = 1.0>
 %     mfs_sphere - Fraction beyond R (eg, 1.3R) for centers <default = 1.3>
+%     BC_choice - How to choose the boundary conditions <default = 1>
+%                 1 : Neumann
+%                 2 : Dirichlet
+%                 3 : Mixed (6 random Dirichlet, the rest Neumann)
+%                 4 : Neumann and one Dirichlet at a reference point (zero 
+%                     potential point)
+%     refpnt - Reference point (zero potential); this is used only if
+%              BC_choice = 4 <default = [0,0,R]>
+%     eval_diff - Consider the solution as the difference between all
+%                 values and a reference point <default = 1>
 %
 %  The value we are interested in studying is the effect of increasing N,
 %  so you must specify a vector of N values that you want to study
@@ -55,6 +65,7 @@
 %                 distribution in a sphere is tricky
 %     errvec_phi - Errors on potential computed at Nvec_true
 %     errvec_J - Errors on current density computed at Nvec_true
+%     condvec - Collocation matrix condition numbers at Nvec_true
 %
 %  Note that if MFS with fewer centers than collocation points is chosen,
 %  condition doesn't make sense (rectangular, not square, system)
@@ -64,12 +75,15 @@ sig = 0.02;
 dipmom = 2.7.*[1, 0, 0];
 srcpnts = [0, 0, 0.6*R];
 
-sol_type = 'kansa'; % mfs not yet tested
+sol_type = 'mfs';
 radbasfun = 'imq';
 point_dist = 'halton';
 ep = 1;
 mfs_frac = 1.0;
 mfs_sphere = 1.3;
+BC_choice = 1;
+refpnt = [0, 0, R];
+eval_diff = 1;
 
 Nvec = 100:100:2000;
 BC_frac = .3; % Not yet implemented
@@ -117,14 +131,32 @@ evalpnts = SphereSurfGoldPoints(N_eval, R);
 % This is the analytic component of the computed solution
 phi_F = phiF_dip(evalpnts,srcpnts,dipmom,sig);
 
-% Analytic solution for the potential (difference)
-% The first evaluation point is taken as reference point
-phi_true = HomSpherePotential(R, sig, srcpnts, dipmom, evalpnts);
-phi_true = phi_true - phi_true(1);
+% Analytic solution for the potential
+phi_an = HomSpherePotential(R, sig, srcpnts, dipmom, evalpnts);
+
+% This is the analytic component of the computed solution for the current
+% density
+Jp = -sig * gradphiF_dip(evalpnts, srcpnts, dipmom, sig);
 
 % Analytic solution for the current density
 J_true = -sig * HomSphereGradient(R, sig, srcpnts, dipmom, evalpnts);
 J_true = (J_true(:,1).^2 + J_true(:,2).^2 + J_true(:,3).^2).^(1/2);
+
+% If requested, compute the difference of the solution with a reference
+% point, arbitrarily chosen as evalpnts(1)
+% Similarly, if Neumann BC with a Dirichlet condition at the reference 
+% point are used, compute the difference of the solution with the reference
+% point 
+if eval_diff || BC_choice == 4
+    if BC_choice == 4 
+        phi_ref = HomSpherePotential(R, sig, srcpnts, dipmom, refpnt);
+        phi_true = phi_an - phi_ref;
+    else
+        phi_true = phi_an - phi_an(1);
+    end
+else
+    phi_true = phi_an;
+end
 
 % Loop through the requested N values
 errvec_phi = [];
@@ -143,7 +175,7 @@ for Npnts = Nvec
     bdydata = POINTS.bdy11;
     N_int = size(intdata,1);
     N_bdy = size(bdydata,1);
-    N_tot = N_int + N_bdy;
+    N_tot = N_int + N_bdy;  % Total number of collocation points
     
     % Compose a vector of all the RBF centers
     % In the MFS setting, these are chosen in a sphere around the ball
@@ -151,7 +183,13 @@ for Npnts = Nvec
         N_ctrs = floor(mfs_frac*Npnts);
         ctrs = SphereSurfGoldPoints(N_ctrs, mfs_sphere*R);
     else % For kansa, the centers and collocation points coincide
-        ctrs = [intdata; bdydata];
+        if BC_choice == 4 % In this case we need an "extra" center
+                          % (reference point)
+            ctrs = [intdata; bdydata; refpnt];
+            N_tot = N_tot + 1;
+        else
+            ctrs = [intdata; bdydata];
+        end
         N_ctrs = N_tot;
     end
     
@@ -164,12 +202,37 @@ for Npnts = Nvec
     DM_eval = DistanceMatrix(evalpnts, ctrs);
     EM = rbf(ep, DM_eval);
     
-    % Neumann BC points
-    bdydata_neu = bdydata;
-    normvecs = NORMALS.n11;
+    % Determine which points are Neumann and which are Dirichlet
+    %   Notice the use of zeros(0,3), not []
+    %   To allow for bdydata_neu(:,1) calls later
+    if BC_choice==1 % Do the standard Neumann BC
+        bdydata_neu = bdydata;
+        normvecs = NORMALS.n11;
+        bdydata_dir = zeros(0,3);
+    elseif BC_choice==2 % Run a test with Dirichlet BC
+        bdydata_neu = zeros(0,3);
+        normvecs = zeros(0,3);
+        bdydata_dir = bdydata;
+    elseif BC_choice==3 % Run a test with Mixed BC
+        % Right now, fixed at 6 Dirichlet BC points
+        % Could be variable, but not important
+        N_dir = min(6,N_bdy);
+        i_dir = randperm(N_bdy,N_dir);
+        i_neu = setdiff(1:N_bdy,i_dir);
+        
+        bdydata_neu = bdydata(i_neu,:);
+        normvecs = NORMALS.n11(i_neu,:);
+        bdydata_dir = bdydata(i_dir,:);
+    else % Run a test with Neumann BC + Dirichlet BC at the reference point
+         % (zero potential point) 
+        bdydata_neu = bdydata;
+        normvecs = NORMALS.n11;
+        bdydata_dir = refpnt;
+    end
     
     % Compute the collocation block for the boundary conditions
     % This also computes the RHS for the problem
+    % First we consider the Neumann BC component
     DM_bdydata_neu = DistanceMatrix(bdydata_neu,ctrs);
 
     % Find all the necessary difference matrices
@@ -187,27 +250,48 @@ for Npnts = Nvec
     % This requires the gradient of the unbounded potential at boundary
     gradphi_F_neu = gradphiF_dip(bdydata_neu, srcpnts, dipmom, sig);
     rhs_bdy_neu = -sum(normvecs.*gradphi_F_neu,2);
-        
+    
+    
+    % Now we consider the Dirichlet BC component
+    DM_bdydata_dir = DistanceMatrix(bdydata_dir,ctrs);
+    BCM_dir = rbf(ep,DM_bdydata_dir);
+    
+    if BC_choice == 4
+        % Dirichlet condition at the reference point (zero potential)
+        rhs_bdy_dir = 0;
+    else
+        % Compute the true solution to be used as Dirichlet BC
+        phi_F_bdy_dir = phiF_dip(bdydata_dir,srcpnts,dipmom,sig);
+        phi_bdy_dir = HomSpherePotential(R, sig, srcpnts, dipmom, bdydata_dir);
+        rhs_bdy_dir = phi_bdy_dir - phi_F_bdy_dir;
+    end
+    
     % Create the full linear system from the blocksand solve it
     % Compose rhs
-    rhs = [rhs_int;rhs_bdy_neu];
+    rhs = [rhs_int;rhs_bdy_neu;rhs_bdy_dir];
     % Compose collocation matrix in same order as rhs
-    CM = [LCM;BCM_neu];
+    CM = [LCM;BCM_neu;BCM_dir];
     % Coefficients for evaluation
     [coefs,recip_cond] = linsolve(CM,rhs);
     
     % Potential at evalpnts in the source free case
     phi0 = EM * coefs;
     % Potential at evalpnts (superposition of effects)
-    phi_comp = phi0 + phi_F;
-    phi_comp = phi_comp - phi_comp(1); 
+    phi = phi0 + phi_F;
+
+    % If requested, compute the difference of the solution with a
+    % reference point, arbitrarily chosen as evalpnts(1)
+    if eval_diff && BC_choice ~= 4 
+        phi_comp = phi - phi(1);
+    else
+        phi_comp = phi;
+    end
     
     % Current density
     Jvol = -sig * gradphi0_dip(ep, coefs, evalpnts, ctrs, radbasfun);
-    Jp = -sig * gradphiF_dip(evalpnts, srcpnts, dipmom, sig);
     J_comp = Jp+Jvol;
     J_comp = (J_comp(:,1).^2 + J_comp(:,2).^2 + J_comp(:,3).^2).^(1/2);
-  
+      
     % Compute the total errors
     errvec_phi(k) = errcompute(phi_comp, phi_true);
     errvec_J(k) = errcompute(J_comp, J_true); 
@@ -235,15 +319,22 @@ if plot_err
         case 4
             errstr = 'RMS Relative Error';
     end
-    
-    bcstr = 'Neumann BC';
-
+    switch BC_choice
+        case 1
+            bcstr = 'Neumann BC';
+        case 2
+            bcstr = 'Dirichlet BC';
+        case 3
+            bcstr = 'Mixed BC';
+        case 4
+            bcstr = 'Zero Reference BC';
+    end
     epstr = sprintf(', \\epsilon=%g',ep);
     
     [AX,H1,H2] = plotyy(Nvec_true,errvec_phi,Nvec_true,errvec_J,@loglog);
     xlabel('Total collocation points')
     set(AX(1),'Xlim',[Nvec_true(1),Nvec_true(end)])
-    set(get(AX(1),'Ylabel'),'String',strcat(errstr, ' on potential difference'))
+    set(get(AX(1),'Ylabel'),'String',strcat(errstr, ' on potential'))
     set(AX(1),'Ycolor',errphicolor)
     set(AX(2),'Xlim',[Nvec_true(1),Nvec_true(end)])
     set(get(AX(2),'Ylabel'),'String',strcat(errstr, ' on current density'))
