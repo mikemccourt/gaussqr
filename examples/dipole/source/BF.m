ea3399403f53edc9ed38b95e407972ae4c546a8d
Salvatore Ganci <salvatore.ganci@unipa.it>
2013-05-09 19:49:45 +0200

    Functions for computation of magnetic field are now commented

--- examples/dipole/source/BF.m
+++ examples/dipole/source/BF.m
@@ -1,38 +1,36 @@
-function BF = BF( evalpnts,srcpnts,dipmom )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function BF = BF( obspnts,srcpnts,dipmom )
+% BF calculates the magnetic (induction) field at M observation points in a
+% homogeneus and infinite medium by superposing the effects of N current 
+% dipoles. 
+% The relative magnetic permeability of the medium is assumed to be 1.
+%
+% Input arguments:
+% obspnts  =  Mx3 matrix of observation points coordinates [m].
+% srcpnts  =  Nx3 matrix of source dipoles coordinates [m].
+% dipmom   =  Nx3 matrix of dipoles moments components [A*m].
+%
+% Output:
+% B        =  Mx3 matrix of magnetic (induction) field at observation 
+%             points [T].
+%
 
-M = size(evalpnts,1);
+% Check input data
+N = size(srcpnts); P = size(dipmom);
+if not(isequal(N,P))
+    error('srcpnts and dipmom dimensions must match')
+end
+
+M = size(obspnts,1);
 BF = zeros(M,3);
 for i = 1:M
-        R = evalpnts(i,:) - srcpnts;
-        R_norm = R(:,1)*R(:,1) + R(:,2)*R(:,2) + R(:,3)*R(:,3);
-        R_norm = R_norm.^(3/2);
-        d = [dipmom(:,2)*R(:,3) - dipmom(:,3)*R(:,2), ...
-             dipmom(:,3)*R(:,1) - dipmom(:,1)*R(:,3), ...
-             dipmom(:,1)*R(:,2) - dipmom(:,2)*R(:,1)];
-        d = bsxfun(@rdivide,d,R_norm);
-        BF(i,:) = sum(d,1);
+    R = bsxfun(@minus,obspnts(i,:),srcpnts);
+    R_norm = R(:,1).*R(:,1)+R(:,2).*R(:,2)+R(:,3).*R(:,3);
+    R_norm = R_norm.^(3/2);
+    d = [dipmom(:,2).*R(:,3) - dipmom(:,3).*R(:,2), ...
+         dipmom(:,3).*R(:,1) - dipmom(:,1).*R(:,3), ...
+         dipmom(:,1).*R(:,2) - dipmom(:,2).*R(:,1)];
+    d = bsxfun(@rdivide,d,R_norm);
+    BF(i,:) = sum(d,1);
 end
 BF = 1e-07*BF;
 end
-
-% function BF = BF( evalpnts,srcpnts,dipmom )
-% %UNTITLED2 Summary of this function goes here
-% %   Detailed explanation goes here
-% 
-% M = size(evalpnts,1); N = size(srcpnts,1);
-% BF = zeros(M,3);
-% for i = 1:M
-%     for j = 1:N
-%         R = evalpnts(i,:) - srcpnts(j,:);
-%         R_norm = R(1)*R(1) + R(2)*R(2) + R(3)*R(3);
-%         R_norm = R_norm^(3/2);
-%         d = [dipmom(j,2)*R(3) - dipmom(j,3)*R(2), ...
-%              dipmom(j,3)*R(1) - dipmom(j,1)*R(3), ...
-%              dipmom(j,1)*R(2) - dipmom(j,2)*R(1)];
-%         BF(i,:) = BF(i,:) + d/R_norm;
-%     end
-% end
-% BF = 1e-07*BF;
-% end
--- examples/dipole/source/Induction.m
+++ examples/dipole/source/Induction.m
@@ -1,24 +1,126 @@
-function B = Induction( r_obs, radius, sig, srcpnts, dipmom, ep, coefs, ctrs, radbasfun, N, sol_type )
+function B = Induction( obspnts,  srcpnts, dipmom, ...
+                        sol_type, radbasfun, ep, ...
+                        coefs, ctrs, ...
+                        sig, radius,  N )
+% Induction calculates the magnetic (induction) field at M observation 
+% points outside the head due to N current dipoles. 
+% The relative magnetic permeability is assumed to be 1.
+%
+% The magnetic field is given by the sum of two terms:
+% - one takes into account the effect of primary currents only. This term
+%   is evaluated by means of an analytical formula.
+% - one takes into account the effect of secondary (volume) currents only.
+%   This term is evaluated by means of numerical integration.
+%
+% Concerning the numerical term, we make use of a corollary of the 
+% Divergence Theorem to integrate the potential function over the 
+% interfaces, instead of the current density (i.e. the gradient of the 
+% potential function) over the entire domain.
+% For details see, e.g., the review on MEG by Hamalainen et al.:
+% Hämäläinen M., et al. "Magnetoencephalography—theory, instrumentation, 
+% and applications to noninvasive studies of the working human brain." 
+% Reviews of Modern Physics 65.2 (1993):413.
+%
+% Note that in MEG literature it is well established that the magnetic
+% field is basically independent of currents flowing in the skull and in
+% the scalp.
+% This fact was observed for the first time in:
+% Hämäläinen M., and Sarvas J. "Realistic conductivity geometry model of 
+% the human head for interpretation of neuromagnetic data." Biomedical 
+% Engineering, IEEE Transactions on 36.2 (1989): 165-171.
+% and widely verified/accepted later on.
+% Therefore, even though we take all the layers into account to evaluate
+% the potential function on the scalp (EEG problem), we can consider only 
+% the innermost interface to evaluate the magnetic field outside the head 
+% (MEG problem) without an appreciable loss in accuracy.
+% This is the reason why this function works with only one layer. We could
+% generalize it if needed.
+%
+% We use (right now) a very simple quasi Monte Carlo integration strategy 
+% with sampling Halton points on the interface. 
+% We need to know surface area in order to evaluate the integral properly.
+%
+% Right now we deal with simple spherical surfaces, so the sampling is
+% straightforward and the interface has known analytical surface area. 
+% For real geometries we have to think about:
+% - the sampling (map points from a unit square to real surface?);
+% - a way of estimating surface areas.
+% It will depend on how we will handle the geometry. Dealing with point
+% clouds only (RBF surfaces interpolation) would be ideal. But also very 
+% rough meshes could be ok since they are easy and fast to build (however 
+% someone could say our method wouldn't be fully meshfree/messfree...)
+%
+% *****NOTE*****
+% This function requires the sample size as input. Of course this "static" 
+% approach is not optimal in general: we should prefer an adaptive strategy 
+% with a certain tolerance provided as input. 
+% It is very easy to modify the code to reach this goal taking advantage of
+% the nested sampling.
+%
+% *****WARNING***** 
+% bsxfun is used to speed up the code. There might be compatibility issues 
+% with old versions of Matlab.
+%
+% Input:
+%   obspnts    = Mx3 matrix of observation points coordinates [m].
+%   srcpnts    = Nx3 matrix of source dipoles coordinates [m].
+%   dipmom     = Nx3 matrix of dipoles moments components [A*m].
+%
+%   sol_type   = Solver type
+%                   'kansa' : Nonsymmetric collocation.
+%                   'mfs' : Method of fundamental solutions.
+%   radbasfun  = RBF for collocation (it matters only if sol_type =
+%                'kansa')
+%   ep         = RBF shape parameter (it matters only if sol_type =
+%                'kansa')
+%
+%   coefs      = coefficients of RBF expansion.
+%   ctrs       = centers for RBF expansion.
+%
+%   sig        = electrical conductivity of the medium [S/m].
+%   radius     = sphere radius [m].
+%   N          = No. of points over the sphere for integration. 
+%
+% Output:
+%   B          = Mx3 matrix of magnetic field at observation points.
+%
+% Calls on: 
+%   pickRBF.m
+%   (haltonseq.m) as an alternative to the MATLAB built-in haltonset
+%   DistanceMatrix.m
+%   BF.m
+%
 
+% Check input data
+P = size(srcpnts); Q = size(dipmom);
+if not(isequal(P,Q))
+    error('srcpnts and dipmom dimensions must match!')
+end
+
+M = size(obspnts,1); % No. of field observation points outside the head
+integral = zeros(M,3); % Inizialize the matrix that will contain the
+                       % integral (numerical) part of the magnetic field
+
+% RBF definition
+% We could think about a better way to call it...
 if strcmp(sol_type,'kansa')
     rbf = pickRBF(radbasfun);
 else
     rbf = pickRBF('fundamental_3d');
 end
 
-
-surf = 4*pi*radius^2; % Sphere surface
-hs = haltonset(2); % 2D Halton set of points (in the unit square)
-M = size(r_obs,1);
-integral = zeros(M,3);
+% Sphere surface area
+surf = 4*pi*radius^2; 
 
 % Source points to be selected on the surface
+hs = haltonset(2); % 2D Halton set of points (in the unit square)
 A = hs(1:N,:);
+% A = haltonseq(1:N,3); % This is an alternative to the MATLAB haltonset
 theta = 2*pi*A(:,1);
 phi = acos(2*A(:,2)-1);
 r_src = [ radius.*sin(phi).*cos(theta), ...
-    radius.*sin(phi).*sin(theta), ...
-    radius.*cos(phi)];
+          radius.*sin(phi).*sin(theta), ...
+          radius.*cos(phi)];
 normals = r_src./radius; % Normal unit vectors
 
 % Evaluate potentials at source points
@@ -27,11 +129,12 @@ DM_eval = DistanceMatrix(r_src, ctrs);
 EM = rbf(ep, DM_eval);
 phi0 = EM * coefs;
 phi = phi0 + phi_F;
-phi = phi - phi(1);
+phi = phi - phi(1); % To evaluate magnetic field the potential difference 
+                    % shouldn't be necessary.
 
 for i = 1:M
     % Distances between source points and field observation point
-    R = bsxfun(@minus,r_obs(i,:),r_src);
+    R = bsxfun(@minus,obspnts(i,:),r_src);
     R_norm = R(:,1).*R(:,1)+R(:,2).*R(:,2)+R(:,3).*R(:,3);
     R_norm = R_norm.^(3/2);
     
@@ -44,64 +147,8 @@ for i = 1:M
     
     integral(i,:) = integral(i,:) + sum(F,1);
 end
-% plot3(r_src(:,1),r_src(:,2),r_src(:,3),'o'); axis square
 B = - 1e-07 * sig * surf * integral / N;
 
-B = B + BF(r_obs,srcpnts,dipmom);
+% Adding the analitycal part
+B = B + BF(obspnts,srcpnts,dipmom);
 end
-
-
-% 
-% 
-% function [ B, res ] = Induction( r_obs, radius, sig, srcpnts, dipmom, ep, coefs, ctrs, radbasfun, reltol, sol_type )
-% 
-% deltaN = 100; % Points to be added at each iteration
-% 
-% bdim = 2*radius; % Bounding cube dimension
-% bbvol = bdim^3;
-% hs = haltonset(3); % 3D Halton set of points (in the unit cube)
-% 
-% N1 = 1;
-% N2 = 500;
-% 
-% M = size(r_obs,1);
-% integral_old = zeros(M,3);
-% integral = integral_old;
-% B = integral_old;
-% res = reltol+eps;
-% % while res >= reltol
-%     % Source points to be selected in a bounding cube containing the domain
-%     r_src = hs(N1:N2,:) * bdim - radius;
-%     
-%     % Select source points inside the domain
-%     r_src = r_src(r_src(:,1).^2 + r_src(:,2).^2 + r_src(:,3).^2 < radius^2, :); 
-% 
-%     % Evaluate current density at source points
-%     J = -sig*gradphiF_dip(r_src, srcpnts, dipmom, sig);
-%     J = J-sig*gradphi0_dip(ep, coefs, r_src, ctrs, radbasfun);
-% 
-%     for i = 1:M
-%         % Distances between source points and field observation point
-%         R = bsxfun(@minus,r_obs(i,:),r_src);
-%         R_norm = R(:,1).*R(:,1)+R(:,2).*R(:,2)+R(:,3).*R(:,3);
-%         R_norm = R_norm.^(3/2);
-% 
-%         % Evaluate the integrand function
-%         F = [ J(:,2).*R(:,3)-J(:,3).*R(:,2), ...
-%               J(:,3).*R(:,1)-J(:,1).*R(:,3), ...
-%               J(:,1).*R(:,2)-J(:,2).*R(:,1)];
-%         F = bsxfun(@rdivide,F,R_norm);
-% 
-%         integral(i,:) = integral(i,:) + sum(F,1);
-%     end
-%     
-%     B_old = B;
-%     B = 1e-07 * bbvol * integral / N2;
-%     
-%     N1 = N2 + 1;
-%     N2 = N2 + deltaN;
-%     
-%     res = norm(B-B_old);
-% % end
-% B = B + BF(r_obs,srcpnts,dipmom);
-% end
