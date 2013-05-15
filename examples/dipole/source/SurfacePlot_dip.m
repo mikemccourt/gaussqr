5d524f67fd74b4c5064aae6fb335df189e05d13d
Salvatore Ganci <salvatore.ganci@unipa.it>
2013-05-15 09:13:44 +0200

    Minor edit: SurfacePlot_dip can now be used to plot the surface only.

--- examples/dipole/source/SurfacePlot_dip.m
+++ examples/dipole/source/SurfacePlot_dip.m
@@ -4,23 +4,33 @@ function h = SurfacePlot_dip( pnts, feval)
 % This function allows you to plot function values on the surface of a
 % sphere.  I guess maybe it works for any manifold, but we only really use
 % it for a sphere.
-%
-% 
+% Delaunay triangulation is used.
+%  
 % Input arguments
-%   pnts - function locations
+%   pnts - function locations. If you pass only this argument then only the
+%          surface will be plotted. This could be useful in order to better 
+%          visualize point distributions on a surface.
 %   feval - function values
 % Output arguments
 %   h - plot handle
 
-% Delaunay triangulation of the sphere
-d_evalpnts = delaunayn(pnts);
-tr = TriRep(d_evalpnts, pnts);
+% Delaunay triangulation of the manifold
+d_pnts = delaunayn(pnts);
+tr = TriRep(d_pnts, pnts);
 faces = freeBoundary(tr);
 
-% figure('Color',[1 1 1]);
-h = patch('Vertices',pnts,'Faces',faces,'FaceVertexCData',feval,...
-      'FaceColor','interp','EdgeColor','none');
-colorbar('FontSize',12);
+if nargin == 1
+    % Plot the surface only
+    h = trisurf(faces,pnts(:,1),pnts(:,2),pnts(:,3),'facecolor',[1,1,0]);
+    colormap(gray(256));
+    shading interp;   
+else
+    % Plot functions values on the surface
+    h = patch('Vertices',pnts,'Faces',faces,'FaceVertexCData',feval,...
+              'FaceColor','interp','EdgeColor','none');
+    colorbar('FontSize',12);
+end
+
 axis('equal'); axis('square'); view(-30,30);
 xlabel('x','FontWeight','bold','FontSize',14);
 ylabel('y','FontWeight','bold','FontSize',14);
