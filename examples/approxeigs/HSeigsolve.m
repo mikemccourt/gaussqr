c1f927a66847b1bb88f1c0d094ab5459c5ea6f3f
Michael McCourt <mccomic@mcs.anl.gov>
2013-06-13 13:27:02 -0500

    Added more explanation and comments

--- examples/approxeigs/HSeigsolve.m
+++ examples/approxeigs/HSeigsolve.m
@@ -2,7 +2,7 @@ function [V,D,A,H] = HSeigsolve(N,basis,rescale,n_eig_plot)
 % This function approximates Hilbert-Schmidt eigenvalues
 % of the Compact Matern kenrel with ep=0 and beta=1
 %
-% function [V,D] = HSeigsolve(N,basis,rescale,n_eig_plot)
+% function PHI = HSeigsolve(N,basis,rescale,n_eig_plot)
 % Inputs : N - number of points in the domain
 %          basis - choice of approximating basis
 %          rescale - <default=1> scale eigenfunctions to sqrt(2)
@@ -21,6 +21,39 @@ function [V,D,A,H] = HSeigsolve(N,basis,rescale,n_eig_plot)
 %    2 - CMatern basis (ep=0,beta=1), Uniform points
 %
 % In this function, we restrict L=1, although we could change that
+%
+% PHI Object details:
+%
+%    PHI.N         : Number of basis functions
+%    PHI.basisName : Which basis is used for the approximation
+%    PHI.centers   : What are the kernel centers for the basis
+%    PHI.indices   : How are the kernels indexed
+%    PHI.basisEval : Function for evaluating the basis
+%                    calling sequence: p = PHI.basisEval(x,z,j)
+%                    x - points at which to evaluate the basis
+%                        x = [x1;x2;x3;...]
+%                    z - centers for the basis
+%                        z = [z1,z2,z3,...]
+%                    j - which basis function to evaluate
+%                        j = [j1,j2,j3,...]
+%                    p - basis function evaluation
+%                        p = [h1(x1),h2(x1),...;h1(x2),h2(x2),...]
+%    PHI.eigvals   : The computed HS eigenvalues
+%    PHI.reorient  : Function to make the eigenfunctions line up
+%                    calling sequence: RV = PHI.basisEval(V)
+%                    V - eigenfunction coefficients
+%                        V = [v1,v2,...]
+%                    RV - reoriented coefficients
+%    PHI.coefs     : Coefficients for evaluating the eigenfunctions
+%                    PHI.coefs(:,k) are for the kth eigenfunction
+%   PHI.eigfuncEval: Function for evaluating the eigenfunctions
+%                    calling sequence: f = PHI.eigfuncEval(x,k)
+%                    x - points at which to evaluate the eigenfunction
+%                        x = [x1;x2;x3;...]
+%                    j - which eigenfunctions to evaluate
+%                        k = [k1,k2,k3,...]
+%                    f - eigenfunction evaluation
+%                        f = [f1(x1),f2(x1),...;f1(x2),f2(x2),...]
 
 L = 1;
 
@@ -52,9 +85,8 @@ switch basis
         
         Int_Kh = @(x,z,j) -1./(j.^2+j).*x.*(x.^j-1);
         H_mat = @(x,z,j) x.^(j-1);
-        Reorient = @(V,z) V*diag(sign(V(2,:)));
+        
         ptspace = 'cheb';
-
         x = pickpoints(0,L,N+2,ptspace);x = x(2:end-1);
         X = repmat(x,1,N);
         z = [];
@@ -70,9 +102,8 @@ switch basis
         Int_Kh = @(x,z,j) 1/3*(x.*z.*(1-max(x,z)).^3 + (1-x).*(1-z).*min(x,z).^3) +...
                           min(x,z).*(1-max(x,z)).*(max(x,z).^2.*(1/2-1/3*max(x,z)) - min(x,z).^2.*(1/2-1/3*min(x,z)));
         H_mat = @(x,z,j) min(x,z) - x.*z;
-        Reorient = @(V) V*diag(sign((1-z)'*V));
+        
         ptspace = 'even';
-
         x = pickpoints(0,L,N+2,ptspace);x = x(2:end-1);
         X = repmat(x,1,N);
         z = x';
@@ -103,8 +134,19 @@ D = D(ix,ix);
 % Store the eigenvalues, now that they've been sorted
 PHI.eigvals = diag(D);
 
-% Reorient the eigenfunctions to have positive derivative at 0
-V = Reorient(V,z);
+
+% Now we need to do some stuff to assume the eigenfunctions can be compared
+% to the analytically known eigenfunctions
+% We need the eigenfunction to have positive derivative at 0
+switch basis
+    case 1
+        PHI.reorient = @(V) V*diag(sign(V(2,:)));
+    case 2
+        PHI.reorient = @(V) V*diag(sign((1-PHI.centers)'*V));
+end
+    
+% Reorient the eigenfunctions
+V = PHI.reorient(V);
 
 % Rescale the eigenfunctions to have max value sqrt(2)
 if rescale~=0
@@ -122,21 +164,16 @@ end
 PHI.coefs       = V;
 PHI.eigfuncEval = @(x,k) PHI.basisEval(x*ones(1,PHI.N),repmat(PHI.centers,size(x,1),1),repmat(PHI.indices,size(x,1),1))*PHI.coefs(:,k);
 
+
+% Now just some cleaning up of other stuff
 % Plot any eigenfunctions the user wants
 if n_eig_plot>0
     Neval = 300;
-    c = V(:,n_eig_plot);
-    
-    xx = pickpoints(0,1,Neval);
-    XX = repmat(xx,1,N);
-    ZZ = repmat(z,Neval,1);
-    JJ = repmat(j,Neval,1);
-    HH = H_mat(XX,ZZ,JJ);
-    P = HH*c;
-    plot(xx,P)
+    xx = pickpoints(0,L,Neval);
+    plot(xx,PHI.eigfuncEval(xx,n_eig_plot))
 end
 
-% Store this information in the PHI object
+% Return the PHI object if the user wants it (which they should)
 if returnPHI
     V = PHI;    
 end
