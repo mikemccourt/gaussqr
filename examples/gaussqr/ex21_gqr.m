e3a7b27264d26b722e60655429431ec0464e9f87
Matt Gliebe <mattgliebe@u.northwestern.edu>
2013-09-13 14:08:33 -0500

    Edits for ex21_gqr; should function now

--- examples/gaussqr/ex21_gqr.m
+++ examples/gaussqr/ex21_gqr.m
@@ -9,31 +9,21 @@ epvec = logspace(-2,1,31);
 N = 15;
 NN = 200;
 x = pickpoints(-1,1,N,'cheb');
-yf = @(x) x+1./(1+x.^2);
-fstring = 'y(x) = x + 1/(1+x^2)';
-% yf = @(x) x.^3-3*x.^2+2*x+1;
-% fstring = 'y(x) = x^3-3x^2+2x+1';
-% yf = @(x) 4*tan(2*x+6);
-% fstring  = 'y(x) = 4tan(2x+6)';
-fstring = sprintf('%s, N = %d',fstring,N);
+fstring = 'b = ';
+fstring = sprintf('%s, %d', fstring, b);
 
 %Here is our vector b
 b      = ones(N,1); %now B is defaulted to some vector
                     % of length N of all ones
 
-y = yf(x); %notice that this is negated below
-xx = pickpoints(-1,1,NN);
-yy = yf(xx);
 alpha = 1;
 lamratio = 1e-12;
 pinvtol = 1e-11;
 
-errvec = [];
 mvec1  = [];
 mvec2  = [];
 mvec3  = [];
 cvec   = [];
-derrvec = [];
 dmvec   = [];
 yPhi    = [];
 yPsi    = [];
@@ -48,16 +38,12 @@ cvecPsi = [];
 
 rbf = @(e,r) exp(-(e*r).^2);
 DM = DistanceMatrix(x,x);
-EM = DistanceMatrix(xx,x);
 
 % Note that yPhi and yPsi are computed with a truncated SVD of Phi1 and Psi
 % respectively.  The tolerance for this is pinvtol and can be set above.
 k = 1;
 for ep=epvec
-    %GQR = gqr_solve(x,y,ep,alpha,2*N+20);
-    GQR = gqr_solveprep(0,x,ep,alpha)
-    %yp = gqr_eval(GQR,xx);
-    %errvec(k) = errcompute(yp,yy);
+    GQR = gqr_solveprep(0,x,ep,alpha,2*N+20);
     
     Phi = gqr_phi(GQR,x);
     Phi1 = Phi(:,1:N);
@@ -72,7 +58,7 @@ for ep=epvec
     lamsave = laminv.*(laminv/laminv(end)>lamratio);    
     Lambda1 = diag(lamvec(1:N));
     Lambda2 = diag(lamvec2);
-    pause
+    
     %approximate y value given b
     y   = Psi*b;
     c = Phi1'\Lambda1\b;
@@ -94,7 +80,7 @@ for ep=epvec
     
     %Mahalanobis Distance Calculation - Method Four (no cancellation; using
     %matrix A which is symmetric and pos. def.
-    mahaldist4 = b'*(diag(lamsave) + ((Phi2')/(Phi1')*diag(lamsave))'*(diag(Lambda2))*(Phi2')/(Phi1')*diag(lamsave))*b;
+    mahaldist4 = b'*(diag(lamsave) + ((Phi2')/(Phi1')*diag(lamsave))'*Lambda2*(Phi2')/(Phi1')*diag(lamsave))*b;
     
     %Mahalanobis Distance Vectors
     mvec1(k) = log(abs(mahaldist1));
@@ -103,10 +89,7 @@ for ep=epvec
     mvec4(k) = log(abs(mahaldist4));
     
     K = rbf(ep, DM);
-    kbasis = rbf(ep,EM);
     warning off
-    yp = kbasis*(K\y);    
-%     derrvec(k) = errcompute(yp,yy);
     
     %Condition vector of matrix K
     cvec(k) = cond(K);
@@ -123,3 +106,40 @@ for ep=epvec
     warning on
     k = k + 1;
 end
+
+
+%Graph 1 - Comparison of Norms with the condition vector
+loglog(epvec, exp(dmvec), '--b', 'linewidth', 3), hold on
+loglog(epvec, cvec, 'g', 'linewidth', 3)
+loglog(epvec, exp(mvec1), 'm', 'linewidth', 3)
+loglog(epvec, exp(mvec2), '--y', 'linewidth', 3)
+loglog(epvec, exp(mvec3), '-.c', 'linewidth', 3)
+loglog(epvec, exp(mvec4), ':r', 'linewidth', 3)
+legend('direct norm','condition vector', 'mvec1', 'mvec2', 'mvec3', 'mvec4')
+xlabel('\epsilon')
+ylabel('Comparison of cond(K), direct norm, mvec1 through mvec4')
+title(fstring), hold off
+figure
+
+%Graph 2 - Comparison of Norms without the condition vector
+loglog(epvec, exp(mvec1), 'm', 'linewidth', 3), hold on
+loglog(epvec, exp(mvec2), '--y', 'linewidth', 3)
+loglog(epvec, exp(mvec3), '-.c', 'linewidth', 3)
+loglog(epvec, exp(mvec4), ':r', 'linewidth', 3)
+loglog(epvec, exp(dmvec), '--b', 'linewidth', 3)
+legend('mvec1', 'mvec2', 'mvec3', 'mvec4', 'dmvec')
+xlabel('\epsilon')
+ylabel('Comparison of Norms')
+title(fstring), hold off
+figure
+
+%Graph 6 - Comparison of Condition Numbers for Phi1, Psi, and K
+loglog(epvec, cvecPhi1, '--g', 'linewidth', 3), hold on
+loglog(epvec, cvecPsi, '--b', 'linewidth', 3)
+loglog(epvec, cvec, '--r', 'linewidth', 3)
+legend('cond(\Phi_1)','cond(\Psi)','cond(K)')
+xlabel('\epsilon')
+ylabel('Comparison of Condition Numbers for \Phi_1, \Psi, and K')
+title(fstring), hold off
+
+beep
