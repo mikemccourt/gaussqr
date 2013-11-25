297f4576760fcec8036e8d563094864fa5f3bfef
Haocheng Bian <hbian1@mcs.anl.gov>
2013-11-25 11:42:24 -0600

    scirpt

---
+++ examples/approxeigs/plot_eigvals.m
@@ -0,0 +1,11 @@
+basis = 3;
+N = 50;
+kernel = 3;
+M = 3;
+figure()
+phigk = HSeigsolveqd(N,kernel,basis,M);
+phicheb = HSeigsolveqd(N,kernel,basis,M);
+semilogy((1:N),phigk.eigvals,'bs')
+
+hold on
+semilogy((1:N),phicheb.eigvals,'r*')
