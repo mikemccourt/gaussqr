64c08939051a6696218648974eda0145517975a8
Michael McCourt <mccomic@mcs.anl.gov>
2014-03-02 18:23:14 -0700

    Basic coupling strategy in, need to work on

--- book/Finance1.m
+++ book/Finance1.m
@@ -62,7 +62,7 @@ payout = @(x) max(0,sum(x-K,2)/d);
 % and far-field locations.  The near-field location is the 0 point and the
 % far-field location is the plane sum(x)=4Kd.  Other boundary conditions
 % are not required to provide well-posedness.
-% Note: This function will return zero everywhere except along the plane 
+% Note: This function will return zero everywhere except along the plane
 % sum(x)==4*K*d, meaning the x=0 BC is automatically built-in
 bc = @(x,t) K*(4-exp(-r*t))*(sum(x,2)==4*K*d);
 
@@ -125,164 +125,162 @@ end
 % almost no effect on the problem
 % Possible ideas include exp(-t) or 1-sqrt(t)
 % NOTE: Not active yet
-coupling = 0;
+coupling = 1;
 coupling_decay = @(t) exp(-7*t/T);
 
 if coupling
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Below this is just a hack, to see if I can get coupling working somewhat
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    x_1 = pickpoints(0,K,ceil(N/2));
+    % I need the number of points in each domain to be different to prevent
+    % singularity in the Jacobian of the algebraic portion of the problem
+    % I think this is just an artifact of the way its set up and not really
+    % important
+    x_2 = pickpoints(K,4*K,ceil(N/2)+1);
+    x_all = [x_1;x_2];
+    N_1 = length(x_1);
+    N_2 = length(x_2);
+    N = N_1 + N_2;
+    delta_x1 = K/(N_1-1);
+    delta_x2 = 3*K/(N_2-1);
+    IN_1 = eye(N_1);
+    IN_2 = eye(N_2);
+    Dx_1 = full(gallery('tridiag',N_1,-1,0,1))/(2*delta_x1);
+    Dx_2 = full(gallery('tridiag',N_2,-1,0,1))/(2*delta_x2);
+    Dxx_1 = -full(gallery('tridiag',N_1))/delta_x1^2;
+    Dxx_2 = -full(gallery('tridiag',N_2))/delta_x2^2;
+    Lint_1 = r*diag(x_1)*Dx_1+.5*B^2*diag(x_1.^2)*Dxx_1-r*IN_1;
+    Lint_2 = r*diag(x_2)*Dx_2+.5*B^2*diag(x_2.^2)*Dxx_2-r*IN_2;
+    L_1 = [IN_1(1,:);Lint_1(2:N_1-1,:);IN_1(N_1,:)];
+    L_2 = [IN_2(1,:);Lint_2(2:N_2-1,:);IN_2(N_2,:)];
+    cp_1_2_dirichlet = [zeros(1,N_1-1),1,-1,zeros(1,N_2-1)];
+    cp_2_1_neumann = [zeros(1,N_1-3),-[-1.5 2 -.5]/delta_x1,[-1.5 2 -.5]/delta_x2,zeros(1,N_2-3)];
+    L_mat = [L_1,zeros(N_1,N_2);zeros(N_2,N_1),L_2];
+    L_mat(N_1:N_1+1,:) = [cp_1_2_dirichlet;cp_2_1_neumann];
+    
+    % Isolate the boundary and coupling conditions
+    %   [Int_1;Int_2;CP_1;CP_2;BC_1;BC_2]
+    %   swap*x = x_isolated
+    IN = eye(N);
+    swap = [IN(2:N_1-1,:);IN(N_1+2:N-1,:);IN(N_1:N_1+1,:);IN(1,:);IN(N,:)];
+    x_all = swap*x_all;
+    L_mat = swap*L_mat*swap';
+    
+    % This will help for plotting
+    i_int = [1:N_1-2,N-3,N_1-1:N-4];
+    x_int = x_all(i_int);
+    
+    % Set up ODE solver for coupling problem
+    % This includes a coupling fix to account for the discontinuous initial
+    % conditions
+    % coupling_decay(t) should start at 1 and decay to 0, so that at T=1 it has
+    % almost no effect on the problem
+    cp_fix = @(t) [zeros(N-3,1);coupling_decay(t);0;0];
+    bc_only = [zeros(N-2,N);zeros(2,N-2),eye(2)];
+    odefun = @(t,u) L_mat*u - bc_only*bc(x_all,t) - cp_fix(t);
+    mass = [eye(N-4),zeros(N-4,4);zeros(4,N)];
+    odeopt = odeset('InitialStep',dt,'Jacobian',L_mat,'Mass',mass,...
+        'MStateDependence','none','MassSingular','yes');
 else
+    % Select some points based on the user requests
+    x = pickpoints(0,4*K,N,pt_opt);
+    N = length(x);
+    
+    % Cut up the domain into pieces to work with
+    % x_bc and x_int are the boundary and interior points
+    % x_eval is a set of points to evaluate the solution on
+    x_int = x(2:end-1);
+    x_bc = x([1,end]);
+    x_all = [x_int;x_bc];
+    i_int = 1:length(x_int);
+    i_bc = length(i_int)+1:length(i_int)+length(x_bc);
+    N_int = length(i_int);
+    N_bc = length(i_bc);
+    N_eval = 300;
+    x_eval = pickpoints(0,4*K,N_eval);
+    
+    % Set up the solver we are using
+    switch solver
+        case 1
+            % Note the dividing factor required to account for the change in scale
+            % we are imposing to normalize our spatial domain to [-1,1]
+            GQR = gqr_solveprep(reg,(x_all-2)/2,ep,alpha);
+            if reg==0
+                Rmat = [eye(N);GQR.Rbar];
+            else
+                Rmat = 1;
+            end
+            RM_all = gqr_phi(GQR,(x_all-2)/2)*Rmat;
+            RM_int = gqr_phi(GQR,(x_int-2)/2)*Rmat;
+            RxM_int = gqr_phi(GQR,(x_int-2)/2,1)*Rmat/2;
+            RxxM_int = gqr_phi(GQR,(x_int-2)/2,2)*Rmat/4;
+        case 0
+            % Create a function for the RBF
+            % The derivatives are needed to create the collocation matrix
+            switch rbf_choice
+                case 1
+                    rbf = @(e,r) exp(-(e*r).^2);
+                    rbfdx = @(e,r,dx) -2*e^2*dx.*exp(-(e*r).^2);
+                    rbfdxx = @(e,r) 2*e^2*(2*(e*r).^2-1).*exp(-(e*r).^2);
+                case 2
+                    rbf = @(e,r) sqrt(1+(e*r).^2);
+                    rbfdx = @(e,r,dx) e^2*dx./sqrt(1+(e*r).^2);
+                    rbfdxx = @(e,r) e^2./(1+(e*r).^2).^(3/2);
+            end
+            % Could also consider the Multiquadrics
+            % We also form distance matrices which we need for evaluation
+            DM_all = DistanceMatrix(x_all,x_all);
+            DM_bc = DistanceMatrix(x_bc,x_all);
+            DM_int = DistanceMatrix(x_int,x_all);
+            DiffM_int = DifferenceMatrix(x_int,x_all);
+            % The basis is evaluated using those distance matrices
+            RM_all = rbf(ep,DM_all);
+            RM_int = rbf(ep,DM_int);
+            RxM_int = rbfdx(ep,DM_int,DiffM_int);
+            RxxM_int = rbfdxx(ep,DM_int);
+        case -1
+            delta_x = 4*K/(N-1);
+            RM_all = 1; % This isn't needed for FD
+            RM_int = eye(N_int,N);
+            RxM_int = ([zeros(N_int,N_bc),eye(N_int)]-eye(N_int,N))/(2*delta_x);
+            RxxM_int = (eye(N_int,N)-2*[zeros(N_int,1),eye(N_int),zeros(N_int,1)]+[zeros(N_int,N_bc),eye(N_int)])/delta_x^2;
+        case -2
+            % Form the Chebyshev differentiation matrices
+            D_cheb = cheb(N);
+            D2_cheb = D_cheb^2;
+            RM_all = 1; % This isn't needed for polynomial collocation
+            RM_int = eye(N_int,N);
+            % Note the division by 2 or 2^2 for the change in scale
+            % cheb expects [-1,1] but we're on [0,4]
+            RxM_int = D_cheb(2:N-1,:)/2;
+            RxxM_int = D2_cheb(2:N-1,:)/4;
+    end
+    
+    % Form the differentiation matrices
+    % Note that only L_mat is used after this block
+    D0 = RM_int/RM_all;
+    Dx = RxM_int/RM_all;
+    Dxx = RxxM_int/RM_all;
+    % Form the differential operator using the differentiation matrices
+    L_mat = r*diag(x_int)*Dx+1/2*B^2*diag(x_int.^2)*Dxx-r*D0;
+    % Swap the columns, if needed for finite differences
+    if solver<0
+        L_mat = L_mat(:,[2:N-1,1,N]);
+    end
+    
+    % We need to set up the ODE solver
+    % The mass matrix is like the identity, but has zeros for the boundary
+    % condtiions, which are not governed by an ODE
+    % The jacobian is exactly what you think it is
+    %   odefun is the L in d/dt u = L u
+    %          on the boundary, it is just u = BC
+    odefun = @(t,u) [L_mat*u;u(i_bc)-bc(x_bc,t)];
+    jac = [L_mat;zeros(N_bc,N_int),eye(N_bc)];
+    mass = [eye(N_int),zeros(N_int,N_bc);zeros(N_bc,N)];
+    odeopt = odeset('Jacobian',jac,'Mass',mass,...
+        'MStateDependence','none','MassSingular','yes');
 end
-
-% Select some points based on the user requests
-x = pickpoints(0,4*K,N,pt_opt);
-N = length(x);
-
-% Cut up the domain into pieces to work with
-% x_bc and x_int are the boundary and interior points
-% x_eval is a set of points to evaluate the solution on
-x_int = x(2:end-1);
-x_bc = x([1,end]);
-x_all = [x_int;x_bc];
-i_int = 1:length(x_int);
-i_bc = length(i_int)+1:length(i_int)+length(x_bc);
-N_int = length(i_int);
-N_bc = length(i_bc);
-N_eval = 300;
-x_eval = pickpoints(0,4*K,N_eval);
-
-% Set up the solver we are using
-switch solver
-    case 1
-    % Note the dividing factor required to account for the change in scale
-    % we are imposing to normalize our spatial domain to [-1,1]
-        GQR = gqr_solveprep(reg,(x_all-2)/2,ep,alpha);
-        if reg==0
-            Rmat = [eye(N);GQR.Rbar];
-        else
-            Rmat = 1;
-        end
-        RM_all = gqr_phi(GQR,(x_all-2)/2)*Rmat;
-        RM_int = gqr_phi(GQR,(x_int-2)/2)*Rmat;
-        RxM_int = gqr_phi(GQR,(x_int-2)/2,1)*Rmat/2;
-        RxxM_int = gqr_phi(GQR,(x_int-2)/2,2)*Rmat/4;
-    case 0
-        % Create a function for the RBF
-        % The derivatives are needed to create the collocation matrix
-        switch rbf_choice
-            case 1
-                rbf = @(e,r) exp(-(e*r).^2);
-                rbfdx = @(e,r,dx) -2*e^2*dx.*exp(-(e*r).^2);
-                rbfdxx = @(e,r) 2*e^2*(2*(e*r).^2-1).*exp(-(e*r).^2);
-            case 2
-                rbf = @(e,r) sqrt(1+(e*r).^2);
-                rbfdx = @(e,r,dx) e^2*dx./sqrt(1+(e*r).^2);
-                rbfdxx = @(e,r) e^2./(1+(e*r).^2).^(3/2);
-        end
-        % Could also consider the Multiquadrics
-        % We also form distance matrices which we need for evaluation
-        DM_all = DistanceMatrix(x_all,x_all);
-        DM_bc = DistanceMatrix(x_bc,x_all);
-        DM_int = DistanceMatrix(x_int,x_all);
-        DiffM_int = DifferenceMatrix(x_int,x_all);
-        % The basis is evaluated using those distance matrices
-        RM_all = rbf(ep,DM_all);
-        RM_int = rbf(ep,DM_int);
-        RxM_int = rbfdx(ep,DM_int,DiffM_int);
-        RxxM_int = rbfdxx(ep,DM_int);
-    case -1
-        delta_x = 4*K/(N-1);
-        RM_all = 1; % This isn't needed for FD
-        RM_int = eye(N_int,N);
-        RxM_int = ([zeros(N_int,N_bc),eye(N_int)]-eye(N_int,N))/(2*delta_x);
-        RxxM_int = (eye(N_int,N)-2*[zeros(N_int,1),eye(N_int),zeros(N_int,1)]+[zeros(N_int,N_bc),eye(N_int)])/delta_x^2;
-    case -2
-        % Form the Chebyshev differentiation matrices
-        D_cheb = cheb(N);
-        D2_cheb = D_cheb^2;
-        RM_all = 1; % This isn't needed for polynomial collocation
-        RM_int = eye(N_int,N);
-        % Note the division by 2 or 2^2 for the change in scale
-        % cheb expects [-1,1] but we're on [0,4]
-        RxM_int = D_cheb(2:N-1,:)/2;
-        RxxM_int = D2_cheb(2:N-1,:)/4;
-end
-
-% Form the differentiation matrices
-% Note that only L_mat is used after this block
-D0 = RM_int/RM_all;
-Dx = RxM_int/RM_all;
-Dxx = RxxM_int/RM_all;
-% Form the differential operator using the differentiation matrices
-L_mat = r*diag(x_int)*Dx+1/2*B^2*diag(x_int.^2)*Dxx-r*D0;
-% Swap the columns, if needed for finite differences
-if solver<0
-    L_mat = L_mat(:,[2:N-1,1,N]);
-end
-
-% We need to set up the ODE solver
-% The mass matrix is like the identity, but has zeros for the boundary
-% condtiions, which are not governed by an ODE
-% The jacobian is exactly what you think it is
-%   odefun is the L in d/dt u = L u
-%          on the boundary, it is just u = BC
-odefun = @(t,u) [L_mat*u;u(i_bc)-bc(x_bc,t)];
-jac = [L_mat;zeros(N_bc,N_int),eye(N_bc)];
-mass = [eye(N_int),zeros(N_int,N_bc);zeros(N_bc,N)];
-odeopt = odeset('Jacobian',jac,'Mass',mass,...
-    'MStateDependence','none','MassSingular','yes');
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Below this is just a hack, to see if I can get coupling working somewhat
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% x_1 = pickpoints(0,K,ceil(N/4));
-% % I need the number of points in each domain to be different to prevent
-% % singularity in the Jacobian of the algebraic portion of the problem
-% % I think this is just an artifact of the way its set up and not really
-% % important
-% x_2 = pickpoints(K,4*K,3*ceil(N/4)+1);
-% x_all = [x_1;x_2];
-% N_1 = length(x_1);
-% N_2 = length(x_2);
-% N = N_1 + N_2;
-% delta_x1 = K/(N_1-1);
-% delta_x2 = 3*K/(N_2-1);
-% IN_1 = eye(N_1);
-% IN_2 = eye(N_2);
-% Dx_1 = full(gallery('tridiag',N_1,-1,0,1))/(2*delta_x1);
-% Dx_2 = full(gallery('tridiag',N_2,-1,0,1))/(2*delta_x2);
-% Dxx_1 = -full(gallery('tridiag',N_1))/delta_x1^2;
-% Dxx_2 = -full(gallery('tridiag',N_2))/delta_x2^2;
-% Lint_1 = r*diag(x_1)*Dx_1+.5*B^2*diag(x_1.^2)*Dxx_1-r*IN_1;
-% Lint_2 = r*diag(x_2)*Dx_2+.5*B^2*diag(x_2.^2)*Dxx_2-r*IN_2;
-% L_1 = [IN_1(1,:);Lint_1(2:N_1-1,:);IN_1(N_1,:)];
-% L_2 = [IN_2(1,:);Lint_2(2:N_2-1,:);IN_2(N_2,:)];
-% cp_1_2_dirichlet = [zeros(1,N_1-1),1,-1,zeros(1,N_2-1)];
-% cp_2_1_neumann = [zeros(1,N_1-3),-[-1.5 2 -.5]/delta_x1,[-1.5 2 -.5]/delta_x2,zeros(1,N_2-3)];
-% L_mat = [L_1,zeros(N_1,N_2);zeros(N_2,N_1),L_2];
-% L_mat(N_1:N_1+1,:) = [cp_1_2_dirichlet;cp_2_1_neumann];
-% 
-% % Isolate the boundary and coupling conditions
-% %   [Int_1;Int_2;CP_1;CP_2;BC_1;BC_2]
-% %   swap*x = x_isolated
-% IN = eye(N);
-% swap = [IN(2:N_1-1,:);IN(N_1+2:N-1,:);IN(N_1:N_1+1,:);IN(1,:);IN(N,:)];
-% x_all = swap*x_all;
-% L_mat = swap*L_mat*swap';
-% 
-% % This will help for plotting
-% i_int = [1:N_1-2,N-3,N_1-1:N-4];
-% x_int = x_all(i_int);
-% 
-% % Set up ODE solver for coupling problem
-% % This includes a coupling fix to account for the discontinuous initial
-% % conditions
-% % coupling_decay(t) should start at 1 and decay to 0, so that at T=1 it has
-% % almost no effect on the problem
-% cp_fix = @(t) [zeros(N-3,1);coupling_decay(t);0;0];
-% bc_only = [zeros(N-2,N);zeros(2,N-2),eye(2)];
-% odefun = @(t,u) L_mat*u - bc_only*bc(x_all,t) - cp_fix(t);
-% mass = [eye(N-4),zeros(N-4,4);zeros(4,N)];
-% odeopt = odeset('InitialStep',dt,'Jacobian',L_mat,'Mass',mass,...
-%     'MStateDependence','none','MassSingular','yes');
 
 % We need to record our initial condition
 u_init = payout(x_all);
