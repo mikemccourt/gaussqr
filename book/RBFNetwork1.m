a894332de7cfe2ef5e510d825fdba2b9815e19fc
Michael McCourt <mccomic@mcs.anl.gov>
2014-05-30 15:07:42 -0600

    Separated first and second examples into two files

--- book/RBFNetwork1.m
+++ book/RBFNetwork1.m
@@ -27,7 +27,7 @@ y = yf(x) + noise*randn(N,1);
 z = pickpoints(-1,1,M,'halton');
 
 % Pick a range of Tikhonov Regularization parameters
-muvec = logspace(-30,10,60);
+muvec = [logspace(-10,-5,5),logspace(-4.9,5,50)];
 
 % For plotting purposes
 xx = pickpoints(-1,1,300);
@@ -36,28 +36,16 @@ yy = yf(xx);
 % Pick a shape parameter and form the design matrix
 % The relevant computations are done with the SVD
 % This is only for stability and can be changed for larger sizes
-ep = .1;
-gqr_alpha = 1;
+ep = 8;
 K_fit = rbf(ep,DistanceMatrix(x,z));
 K_predict = rbf(ep,DistanceMatrix(xx,z));
-[UK,SK,VK] = svd(K_fit,0);SKv = diag(SK);
-
-% Also, form necessary GQR stuff
-% Note that we are feeding this the centers, with which to form the stable
-% basis, and also the eigenfunction basis for comparison
-GQR = gqr_solveprep(0,z,ep,gqr_alpha);
-Psi = gqr_phi(GQR,x)*[eye(M);GQR.Rbar];
-[UPsi,SPsi,VPsi] = svd(Psi,0);SPsiv = diag(SPsi);
-% This is the eigenfunction basis
-GQR_reg = gqr_solveprep(1,z,ep,gqr_alpha,M);
-Phi_reg = gqr_phi(GQR_reg,x);
-[UPhi,SPhi,VPhi] = svd(Phi_reg,0);SPhiv = diag(SPhi);
+[U,S,V] = svd(K_fit,0);Sv = diag(S);
 
 % Conduct the loop over the regularization values
-dirvec = [];gqrvec = [];eigvec = [];
-loovec = [];loevec = [];
-gcvvec = [];gcevec = [];
-eig_err_best = Inf;dir_err_best = Inf;gce_err_best = Inf;gcd_err_best = [];
+errvec = [];
+loovec = [];
+gcvvec = [];
+err_min = Inf;gcv_min = Inf;loo_min = Inf;
 k = 1;
 for mu=muvec
     % Solve for the newtork weights, here with the standard basis
@@ -66,7 +54,7 @@ for mu=muvec
     % Evaluate predictions on the test/plotting points
     % Check the error
     yp = K_predict*w;
-    dirvec(k) = errcompute(yp,yy);
+    errvec(k) = errcompute(yp,yy);
 
     % Find the projection matrix, used to evaluated the residual
     P = eye(N) - UK*diag(1./(1+mu./SKv.^2))*UK';
@@ -74,80 +62,51 @@ for mu=muvec
 
     % Evaluate the parameterization schemes
     % The projection matrix is needed for this as well
-    lodvec(k) = Py'*diag(1./diag(P).^2)*Py/N;
-    gcdvec(k) = N*Py'*Py/trace(P)^2;
-    
-    % Compute instead the coefficients for the HS-SVD method
-    % We will first compute with the stable basis
-    GQR.coef = VPsi*((UPsi'*y)./(SPsiv+mu./SPsiv));
-    yp = gqr_eval(GQR,xx);
-    gqrvec(k) = errcompute(yp,yy);
-    
-    % Compute as well with the eigenfunctions
-    % First the CV content for the eigenfunction basis
-    % Then the predictions at the test points
-    GQR_reg.coef = VPhi*((UPhi'*y)./(SPhiv+mu./SPhiv));
-    P = eye(N) - UPhi*diag(1./(1+mu./SPhiv.^2))*UPhi';
-    Py = P*y;
-    gcevec(k) = N*Py'*Py/trace(P)^2;
-    loevec(k) = Py'*diag(1./diag(P).^2)*Py/N;
-    yp = gqr_eval(GQR_reg,xx);
-    eigvec(k) = errcompute(yp,yy);
-
-    if eigvec(k)<eig_err_best
-        eig_err_best = eigvec(k);eig_mu_best = mu;eig_y_best = yp;
+    loovec(k) = Py'*diag(1./diag(P).^2)*Py/N;
+    gcvvec(k) = N*Py'*Py/trace(P)^2;
+    if errvec(k)<err_min
+        err_min = errvec(k);err_mu_best = mu;err_y_best = yp;
     end
-    if dirvec(k)<dir_err_best
-        dir_err_best = dirvec(k);dir_mu_best = mu;dir_y_best = yp;
+    if gcvvec(k)<gcv_min
+        gcv_min = gcvvec(k);gcv_mu_best = mu;gcv_y_best = yp;
     end
-    if gcevec(k)<gce_err_best
-        gce_err_best = gcevec(k);gce_mu_best = mu;gce_y_best = yp;
-    end
-    if gcdvec(k)<gce_err_best
-        gcd_err_best = gcdvec(k);gcd_mu_best = mu;gcd_y_best = yp;
+    if loovec(k)<loo_min
+        loo_min = loovec(k);loo_mu_best = mu;loo_y_best = yp;
     end
     k = k + 1;
 end
 
-[tmp,id] = min(dirvec);
-[tmp,ig] = min(gcdvec);
-[tmp,ie] = min(eigvec);
-[tmp,ic] = min(gcevec);
+[tmp,id] = min(errvec);
+[tmp,ig] = min(gcvvec);
+[tmp,il] = min(loovec);
+handles = [];
 figure
-handles(1) = loglog(muvec,dirvec,'linewidth',3);
+handles(1) = loglog(muvec,errvec,'k','linewidth',3);
 hold on
-handles(2) = loglog(muvec,gcdvec,'--','linewidth',3);
-handles(3) = loglog(muvec,eigvec,'r','linewidth',3);
-handles(4) = loglog(muvec,gcevec,'--r','linewidth',3);
-loglog(muvec(id),dirvec(id),'x','linewidth',3,'markersize',16)
-loglog(muvec(ig),gcdvec(ig),'x','linewidth',3,'markersize',16)
-loglog(muvec(ie),eigvec(ie),'r+','linewidth',3,'markersize',16)
-loglog(muvec(ic),gcevec(ic),'r+','linewidth',3,'markersize',16)
-handles(5) = loglog(muvec(1:2:end),gqrvec(1:2:end),'ok','linewidth',1);
+handles(2) = loglog(muvec,gcvvec,'--','linewidth',3);
+handles(3) = loglog(muvec,loovec,'-.','color',[.7 .5 0],'linewidth',3);
+loglog(muvec(id),errvec(id),'k+','linewidth',3,'markersize',16)
+loglog(muvec(ig),gcvvec(ig),'x','linewidth',3,'markersize',16)
+loglog(muvec(il),loovec(il),'x','color',[.7 .5 0],'linewidth',3,'markersize',16)
 title(sprintf('ep=%g,N=%d,M=%d',ep,N,M))
 xlabel('\mu')
-legend(handles,'Standard Basis','Standard GCV',...
-       'Eigenfunction Basis','Eigenfunction GCV',...
-       'Stable Basis','location','northwest')
+legend(handles,'Error','GCV','LOOCV','location','northwest')
 hold off
 
-% This can compute the mu=0 error, which I guess should be mu now that
-% I think about it
-GQR_reg.coef = Phi_reg\y;
-yp = gqr_eval(GQR_reg,xx);
-mu0_err = errcompute(yp,yy);
+% Store the error (not the min) for the best GCV
+gcv_min_err = errcompute(gcv_y_best,yy);
 
 % Plot the results
 figure
 plot(x,y,'or')
 hold on
-plot(xx,yy,'linewidth',2)
-plot(xx,eig_y_best,'--k','linewidth',2)
-plot(xx,dir_y_best,'-.m','linewidth',2)
+plot(xx,yy,'r','linewidth',2)
+plot(xx,err_y_best,'k','linewidth',2)
+plot(xx,gcv_y_best,'--','linewidth',2)
 hold off
-ylim([-1,2])
-title(sprintf('ep=%g,mu=%g,N=%d,M=%d',ep,eig_mu_best,N,M))
+ylim([-.5,2])
+title(sprintf('ep=%g,mu=%g,N=%d,M=%d',ep,gcv_mu_best,N,M))
 legend('Data','True',...
-    sprintf('Eig Opt err=%2.2g',eig_err_best),...
-    sprintf('Stand Opt err=%2.2g',dir_err_best),...
-    'location','south')
+    sprintf('Min Error err=%2.2g',err_min),...
+    sprintf('Min GCV err=%2.2g',gcv_min_err),...
+    'location','northeast')
--- book/RBFNetwork2.m
+++ book/RBFNetwork2.m
@@ -1,6 +1,6 @@
-% RBFNetwork2
-% This example considers a fixed number of basis functions and studies the
-% effect of varying ep and lam values
+% RBFNetwork1
+% This first example considers Tikhonov Regularization for fixed centers
+% and shape parameters
 
 % Initial example for support-vector machines
 if exist('rng','builtin')
@@ -10,115 +10,144 @@ else
     randn('state',0);
 end
 
+% Testing data
 N = 50;
+M = 15;
 yf = @(x) (1-4*x+32*x.^2).*exp(-16*x.^2);
 rbf = @(e,r) exp(-(e*r).^2);
 
 % Pick points to evaluate the function at
 % Add some error to the data
-% Make sure the end points are included because we can't interpolate beyond
-% the end points - that would be extrapolation
 x = pickpoints(-1,1,N-2,'rand');
 x = [x;-1;1];
 noise = .2;
 y = yf(x) + noise*randn(N,1);
 
+% Choose points at which to center the basis functions, as needed
+z = pickpoints(-1,1,M,'halton');
+
+% Pick a range of Tikhonov Regularization parameters
+muvec = logspace(-30,10,60);
+
 % For plotting purposes
 xx = pickpoints(-1,1,300);
 yy = yf(xx);
 
-% Pick a range of shape and regularization parameters
+% Pick a shape parameter and form the design matrix
+% The relevant computations are done with the SVD
+% This is only for stability and can be changed for larger sizes
+ep = .1;
 gqr_alpha = 1;
-N_ep = 30;
-N_lam = 35;
-epvec = logspace(-2,1,N_ep);
-lamvec = logspace(-10,5,N_lam);
-Mvec = [10,15,20];
+K_fit = rbf(ep,DistanceMatrix(x,z));
+K_predict = rbf(ep,DistanceMatrix(xx,z));
+[UK,SK,VK] = svd(K_fit,0);SKv = diag(SK);
 
-m = 1;
-err_best = Inf;
-for M=Mvec
-    % Choose points at which to center the basis functions
-    z = pickpoints(-1,1,M);
+% Also, form necessary GQR stuff
+% Note that we are feeding this the centers, with which to form the stable
+% basis, and also the eigenfunction basis for comparison
+GQR = gqr_solveprep(0,z,ep,gqr_alpha);
+Psi = gqr_phi(GQR,x)*[eye(M);GQR.Rbar];
+[UPsi,SPsi,VPsi] = svd(Psi,0);SPsiv = diag(SPsi);
+% This is the eigenfunction basis
+GQR_reg = gqr_solveprep(1,z,ep,gqr_alpha,M);
+Phi_reg = gqr_phi(GQR_reg,x);
+[UPhi,SPhi,VPhi] = svd(Phi_reg,0);SPhiv = diag(SPhi);
+
+% Conduct the loop over the regularization values
+dirvec = [];gqrvec = [];eigvec = [];
+loovec = [];loevec = [];
+gcdvec = [];gcevec = [];
+eig_err_best = Inf;dir_err_best = Inf;gce_err_best = Inf;gcd_err_best = [];
+k = 1;
+for mu=muvec
+    % Solve for the newtork weights, here with the standard basis
+    w = VK*((UK'*y)./(SKv+mu./SKv));
+
+    % Evaluate predictions on the test/plotting points
+    % Check the error
+    yp = K_predict*w;
+    dirvec(k) = errcompute(yp,yy);
+
+    % Find the projection matrix, used to evaluated the residual
+    P = eye(N) - UK*diag(1./(1+mu./SKv.^2))*UK';
+    Py = P*y;
+
+    % Evaluate the parameterization schemes
+    % The projection matrix is needed for this as well
+    lodvec(k) = Py'*diag(1./diag(P).^2)*Py/N;
+    gcdvec(k) = N*Py'*Py/trace(P)^2;
     
-    dirmat = zeros(N_ep,N_lam);
-    gqrmat = zeros(N_ep,N_lam);
-    eigmat = zeros(N_ep,N_lam);
-    k = 1;
-    for ep=epvec
-        % Pick a shape parameter and form the design matrix
-        H = rbf(ep,DistanceMatrix(x,z));
+    % Compute instead the coefficients for the HS-SVD method
+    % We will first compute with the stable basis
+    GQR.coef = VPsi*((UPsi'*y)./(SPsiv+mu./SPsiv));
+    yp = gqr_eval(GQR,xx);
+    gqrvec(k) = errcompute(yp,yy);
+    
+    % Compute as well with the eigenfunctions
+    % First the CV content for the eigenfunction basis
+    % Then the predictions at the test points
+    GQR_reg.coef = VPhi*((UPhi'*y)./(SPhiv+mu./SPhiv));
+    P = eye(N) - UPhi*diag(1./(1+mu./SPhiv.^2))*UPhi';
+    Py = P*y;
+    gcevec(k) = N*Py'*Py/trace(P)^2;
+    loevec(k) = Py'*diag(1./diag(P).^2)*Py/N;
+    yp = gqr_eval(GQR_reg,xx);
+    eigvec(k) = errcompute(yp,yy);
 
-        % Also, form necessary GQR stuff
-        % Note that we are feeding this the centers, with which to form the stable
-        % basis, and also the eigenfunction basis for comparison
-        GQR = gqr_solveprep(1,z,ep,gqr_alpha,M);
-        Phi = gqr_phi(GQR,x);
-
-        j = 1;
-        for lam=lamvec
-            % Form the variance matrix and solve for the weights
-            % This uses the direct method
-            A = H'*H + lam*eye(M);
-            w = A\(H'*y);
-
-            % Evaluate predictions on the test/plotting points
-            % Check the error
-            yp = rbf(ep,DistanceMatrix(xx,z))*w;
-            dirmat(k,j) = errcompute(yp,yy);
-
-            % Compute as well with the eigenfunctions
-            GQR.coef = (Phi'*Phi + lam*eye(M))\(Phi'*y);
-            yp = gqr_eval(GQR,xx);
-            eigmat(k,j) = errcompute(yp,yy);
-
-            if eigmat(k,j)<err_best
-                err_best = eigmat(k,j);
-                M_best = M;
-                ep_best = ep;
-                lam_best = lam;
-                y_best = yp;
-            end
-            j = j + 1;
-        end
-        k = k + 1;
+    if eigvec(k)<eig_err_best
+        eig_err_best = eigvec(k);eig_mu_best = mu;eig_y_best = yp;
     end
-
-    % Plot the errors of the various solution strategies
-    [E,L] = meshgrid(epvec,lamvec);
-    subplot(2,3,m)
-    h = surf(E,L,log10(dirmat'));
-    title(sprintf('standard,N=%d,M=%d',N,M))
-    set(h,'edgecolor','none')
-    set(gca,'xscale','log');set(gca,'yscale','log');
-    xlim([1e-2,1e1]),ylim([1e-10,1e5]),zlim([-2.5,-1])
-    xlabel('\epsilon'),ylabel('\lambda'),zlabel('log_{10} error')
-    subplot(2,3,m+length(Mvec))
-    h = surf(E,L,log10(eigmat'));
-    title(sprintf('eigs,N=%d,M=%d',N,M))
-    set(h,'edgecolor','none')
-    set(gca,'xscale','log');set(gca,'yscale','log');
-    xlim([1e-2,1e1]),ylim([1e-10,1e5]),zlim([-2.5,-1])
-    xlabel('\epsilon'),ylabel('\lambda'),zlabel('log_{10} error')
-    
-    m = m + 1
+    if dirvec(k)<dir_err_best
+        dir_err_best = dirvec(k);dir_mu_best = mu;dir_y_best = yp;
+    end
+    if gcevec(k)<gce_err_best
+        gce_err_best = gcevec(k);gce_mu_best = mu;gce_y_best = yp;
+    end
+    if gcdvec(k)<gcd_err_best
+        gcd_err_best = gcdvec(k);gcd_mu_best = mu;gcd_y_best = yp;
+    end
+    k = k + 1;
 end
 
-% Also solve the problem in the flat limit, with no regularization
-GQR = gqr_solveprep(1,z,1e-8,gqr_alpha,M);
-Phi = gqr_phi(GQR,x);
-GQR.coef = Phi\y;
-yp = gqr_eval(GQR,xx);
-lamep0_err = errcompute(yp,yy);
+[tmp,id] = min(dirvec);
+[tmp,ig] = min(gcdvec);
+[tmp,ie] = min(eigvec);
+[tmp,ic] = min(gcevec);
+figure
+handles(1) = loglog(muvec,dirvec,'linewidth',3);
+hold on
+handles(2) = loglog(muvec,gcdvec,'--','linewidth',3);
+handles(3) = loglog(muvec,eigvec,'r','linewidth',3);
+handles(4) = loglog(muvec,gcevec,'--r','linewidth',3);
+loglog(muvec(id),dirvec(id),'x','linewidth',3,'markersize',16)
+loglog(muvec(ig),gcdvec(ig),'x','linewidth',3,'markersize',16)
+loglog(muvec(ie),eigvec(ie),'r+','linewidth',3,'markersize',16)
+loglog(muvec(ic),gcevec(ic),'r+','linewidth',3,'markersize',16)
+handles(5) = loglog(muvec(1:2:end),gqrvec(1:2:end),'ok','linewidth',1);
+title(sprintf('ep=%g,N=%d,M=%d',ep,N,M))
+xlabel('\mu')
+legend(handles,'Standard Basis','Standard GCV',...
+       'Eigenfunction Basis','Eigenfunction GCV',...
+       'Stable Basis','location','northwest')
+hold off
+
+% This can compute the mu=0 error, which I guess should be mu now that
+% I think about it
+GQR_reg.coef = Phi_reg\y;
+yp = gqr_eval(GQR_reg,xx);
+mu0_err = errcompute(yp,yy);
 
 % Plot the results
 figure
 plot(x,y,'or')
 hold on
 plot(xx,yy,'linewidth',2)
-plot(xx,y_best,'--k','linewidth',2)
-plot(xx,yp,'-.m','linewidth',2)
+plot(xx,eig_y_best,'--k','linewidth',2)
+plot(xx,dir_y_best,'-.m','linewidth',2)
 hold off
 ylim([-1,2])
-title(sprintf('N=%d,M=%d',N,M))
-legend('Data','True',sprintf('M=%d lam=%g ep=%g err=%g',M_best,lam_best,ep_best,err_best),sprintf('M=%d lam=ep=0 err=%g',M,lamep0_err))
+title(sprintf('ep=%g,mu=%g,N=%d,M=%d',ep,eig_mu_best,N,M))
+legend('Data','True',...
+    sprintf('Eig Opt err=%2.2g',eig_err_best),...
+    sprintf('Stand Opt err=%2.2g',dir_err_best),...
+    'location','south')
