892e5879335f5ab01e0b478822d1af014c9625cb
Michael McCourt <mccomic@mcs.anl.gov>
2014-06-06 01:12:16 -0600

    Criteria for plotting function (must write it)

---
+++ book/SaveFig.m
@@ -0,0 +1,38 @@
+
+% This function saves the active plot handle with the standard book format
+% Black & White
+%   Rendering
+%      Colorspace black and white
+%      Custom color w
+%      Resolution 600
+%      Keep axis limits
+%      Show uicontrols
+%   Fonts
+%      Custom - fixed font size 14
+%      Custom name - Helvetica ?
+%   Lines
+%      Fixed width 2 points
+% Color
+%   Rendering
+%      Colorspace CMYK
+%      Custom color w
+%      Resolution 300
+%      Keep axis limits
+%      Show uicontrols
+%   Fonts
+%      Custom - fixed font size 14
+%      Custom name - Helvetica ?
+%   Lines
+%      Fixed width 2 points
+% Grayscale
+%   Rendering
+%      Colorspace grayscale
+%      Custom color w
+%      Resolution 300
+%      Keep axis limits
+%      Show uicontrols
+%   Fonts
+%      Custom - fixed font size 14
+%      Custom name - Helvetica ?
+%   Lines
+%      Fixed width 2 points
