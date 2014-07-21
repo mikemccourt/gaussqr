66ddc8a71934677f8720b43d81e7d9940cc8855e
Michael McCourt <mccomic@mcs.anl.gov>
2014-07-21 08:44:54 -0500

    Update to GSS example to allow for missing boundary data

--- book/GSSEx1.m
+++ book/GSSEx1.m
@@ -1,6 +1,7 @@
 f = @(x) sin(2*pi*x) + x;
-N = 30;
+N = 10;
 x = pickpoints(0,1,N);
+x = pickpoints(0,1,N+2);x = x(2:end-1);
 y = f(x);
 NN = 150;
 xx = pickpoints(0,1,NN);
@@ -15,17 +16,17 @@ XX = tmp{1};
 ZZ = tmp{2};
 
 KK{1} = @(x,z) min(x,z) - .129*x.*z   - .3261*(x + z)  + .2656;
-KK{2} = @(x,z) min(x,z) - 1.5235*x.*z + .0448*(x + z)  + .0838;
-KK{3} = @(x,z) min(x,z) - 5.317*x.*z  - 2.1692*(x + z) - 1.0958;
+KK{2} = @(x,z) min(x,z) - 0.3598*x.*z - 0.2055*(x + z) + 0.1376;
+KK{3} = @(x,z) min(x,z) + 3.3171*x.*z - 2.1692*(x + z) + 1.1006;
 KK{4} = @(x,z) min(x,z) + .998*x.*z   - .9988*(x + z)  + .9999;
 
-errvec = zeros(size(K));
 h = figure;
 for k=1:4
     Kf = KK{k};
     K_int = Kf(X,Z);
     K_eval = Kf(XX,ZZ);
     yp = K_eval*(K_int\y);
+    
     subplot(2,2,k)
     plot(xx,yp,'linewidth',3)
     title(sprintf('Error = %g',errcompute(yp,yy)))
