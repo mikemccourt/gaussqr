fee32da8de6412461c1e99424160b0fad52628fa
Michael McCourt <mccomic@mcs.anl.gov>
2014-07-22 15:53:51 -0500

    Added Neumann BC to test case, not totally confident yet

--- examples/dipole/disksourcetest_MFS_couple.m
+++ examples/dipole/disksourcetest_MFS_couple.m
@@ -6,16 +6,18 @@
 %          u = v,     boundary
 
 % The Dirichlet boundary condition function
-%v = @(x) 1 + x(:,1) + x(:,2);
+% v = @(x) 1 + x(:,1) + x(:,2);
 % v = @(x) x(:,1) + x(:,2) + 1./((x(:,1)-2).^2 + x(:,2).^2);
 v = @(x) x(:,1) + x(:,2) + log((x(:,1)-2).^2 + x(:,2).^2);
+vx = @(x) 1 + 2*(x(:,1)-2)./((x(:,1)-2).^2 + x(:,2).^2);
+vy = @(x) 1 + 2*x(:,2)./((x(:,1)-2).^2 + x(:,2).^2);
 
 % The fundamental solution
 fs = @(x,z) log(DistanceMatrix(x,z));
 fsd = @(x,z,dim) DifferenceMatrix(x(:,dim),z(:,dim))./(DistanceMatrix(x,z).^2);
 
-% Error to introduce into the boundary condition (0 for no noise)
-noise = 0.0001;
+% Choose to use the Neumann BC instead
+neumannBC = 0;
 
 % Circle radii
 % We're going to do a coupling between an inner circle and outer ring
@@ -42,6 +44,9 @@ Nvec = floor(logspace(1,3,20));
 errvec = zeros(size(Nvec));
 coefvec = zeros(size(Nvec));
 condvec = zeros(size(Nvec));
+oldtest = zeros(NN,1);
+newtest = zeros(NN,1);
+errdvec = zeros(size(Nvec));
 k = 1;
 for N=Nvec
     % Define the number of points and the domains we are working on
@@ -63,26 +68,42 @@ for N=Nvec
     %   [ K(xiI,zo)  -K(xiI,zi)] [    ] = [ 0 ]
     %   [GK(xiD,zo) -GK(xiK,zi)] [ ci ] = [ 0 ]
     % Also needed is the directional derivative in G
-    H = [[fs(xo,zo),zeros(N)];
-        [fs(xiI,zo),-fs(xiI,zi)];
-        [diag(xiD(:,1)/ri)*fsd(xiD,zo,1)+diag(xiD(:,2)/ri)*fsd(xiD,zo,2),diag(xiD(:,1)/ri)*fsd(xiD,zi,1)+diag(xiD(:,2)/ri)*fsd(xiD,zo,2)]];
-
+    if neumannBC
+        iN = 1:2:N;
+        iD = 2:2:N;
+        H = [[fs(xo(iD,:),zo),zeros(length(iD),N)];
+            [diag(xo(iN,1)/ro)*fsd(xo(iN,:),zo,1)+diag(xo(iN,2)/ro)*fsd(xo(iN,:),zo,2),zeros(length(iN),N)];
+            [fs(xiI,zo),-fs(xiI,zi)];
+            [diag(xiD(:,1)/ri)*fsd(xiD,zo,1)+diag(xiD(:,2)/ri)*fsd(xiD,zo,2),diag(xiD(:,1)/ri)*fsd(xiD,zi,1)+diag(xiD(:,2)/ri)*fsd(xiD,zo,2)]];
+        rhs = [v(xo(iD,:));diag(xo(iN,1)/ro)*vx(xo(iN,:))+diag(xo(iN,2)/ro)*vy(xo(iN,:));zeros(length(xiI),1);zeros(length(xiD),1)];
+    else
+        H = [[fs(xo,zo),zeros(N)];
+            [fs(xiI,zo),-fs(xiI,zi)];
+            [diag(xiD(:,1)/ri)*fsd(xiD,zo,1)+diag(xiD(:,2)/ri)*fsd(xiD,zo,2),diag(xiD(:,1)/ri)*fsd(xiD,zi,1)+diag(xiD(:,2)/ri)*fsd(xiD,zo,2)]];
+        rhs = [v(xo);zeros(length(xiI),1);zeros(length(xiD),1)];
+    end
     K_eval = fs(xx,zo);
-    rhs = [v(xo);zeros(length(xiI),1);zeros(length(xiD),1)];
 
     % Solve the system and compute the errors
     % Only using the coefficients for evaluating on the boundary 
     full_coef = H\rhs;
     outer_coef = full_coef(1:N);
-    yp = K_eval*outer_coef;
-    errvec(k) = errcompute(yp,yy);
+    newtest = K_eval*outer_coef;
+    errvec(k) = errcompute(newtest,yy);
     condvec(k) = cond(H);
     coefvec(k) = norm(outer_coef);
+    if k>1
+        errdvec(k) = errcompute(oldtest,newtest);
+        if k==2
+            errdvec(1) = errdvec(2);
+        end
+    end
+    oldtest = newtest;
     k = k + 1;
 end
 
 h = figure;
-loglog(Nvec,[errvec;condvec;coefvec],'linewidth',3)
+loglog(Nvec,[errvec;condvec;coefvec;errdvec],'linewidth',3)
 xlabel('Boundary collocation points')
-legend('Error','Cond','Coef Norm')
+legend('Error','Cond','Coef Norm','Test Diff')
 title('Coupling')
