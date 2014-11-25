dd2b1f4f6a2f5a81c60e34d88434b405dc38e369
Michael McCourt <mccomic@mcs.anl.gov>
2014-11-24 20:14:55 -0700

    Working on 1D CDF problem

--- book/SurrModel4.m
+++ book/SurrModel4.m
@@ -1,6 +1,6 @@
 % SurrModel4.m
 % This considers the development of a surrogate model for the empirical
-% distribution function (ECDF) of data drawn from a random distribution.
+% distribution function (EDF) of data drawn from a random distribution.
 % The data we consider again comes from the carsmall data set, where we
 % consider the probability of finding a car with a given set of
 % Acceleration, Displacement, Horsepower and Weight values.
@@ -9,10 +9,131 @@
 % possible Acceleration and Displacement values which occur with
 % probability density defined through the ECDF surrogate model.
 
-N = 200;
+% Standardize the random results
+global GAUSSQR_PARAMETERS
+GAUSSQR_PARAMETERS.RANDOM_SEED(0);
+
+% Define some RBFs for use on this problem
+rbfM2 = @(r) (1+r).*exp(-r);
+rbfM2dx = @(r,dx,ep) -ep^2*exp(-r).*dx;
+rbfM2dxdy = @(r,dx,dy,ep) prod(ep.^2)*exp(-r).*dx.*dy./(r+eps);
+rbfM4 = @(r) (3+3*r+r.^2).*exp(-r);
+rbfM4dx = @(r,dx,ep) -ep^2*exp(-r).*(1+r).*dx;
+rbfM4dxdy = @(r,dx,dy,ep) prod(ep.^2)*exp(-r).*dx.*dy;
+rbfM6 = @(r) (15+15*r+6*r.^2+r.^3).*exp(-r);
+rbfM6dx = @(r,dx,ep) -ep^2*exp(-r).*(r.^2+3*r+3).*dx;
+rbfM6dxdy = @(r,dx,dy,ep) prod(ep.^2)*exp(-r).*dx.*dy.*(1+r);
+
+% This function allows you to evaluate the EDF
+% Here, xe are the evaluation points, x are the observed locations
+Fhat = @(xe,x) reshape(sum(all(repmat(x,[1,1,size(xe,1)])<=repmat(reshape(xe',[1,size(xe,2),size(xe,1)]),[size(x,1),1,1]),2),1),size(xe,1),1)/size(x,1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Below is a 1D example for creating an EDF response surface
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Create some random samples from a beta random variable, a=4, b=3
+beta_a = 2;
+beta_b = 2;
+N = 440;
+x = betainv(rand(N,1),beta_a,beta_b);
+
+% Evaluate the EDF at some points to create data for the model
+Ndata = 700;
+xdata = pickpoints(0,1,Ndata);
+ydata = Fhat(xdata,x);
+
+% Plot the EDF from the randomly generated data
+h_cdf_ex = figure;
+subplot(1,3,1)
+plot(xdata,ydata,'linewidth',3)
+title('Empirical CDF')
+
+% Choose an RBF to work with
+rbf = rbfM4;
+rbfdx = rbfM4dx;
+
+% Create the surrogate model
+ep = 4;
+mu = 1e-1;
+K_cdf = rbf(DistanceMatrix(xdata,xdata,ep));
+cdf_coef = (K_cdf+mu*eye(Ndata))\ydata;
+cdf_eval = @(xeval) rbf(DistanceMatrix(xeval,xdata,ep))*cdf_coef;
+pdf_eval = @(xeval) rbfdx(DistanceMatrix(xeval,xdata,ep),DifferenceMatrix(xeval,xdata),ep)*cdf_coef;
+
+% Evaluate and plot the surrogate CDF
+Nplot = 400;
+xplot = pickpoints(0,1,Nplot);
+cplot = cdf_eval(xplot);
+subplot(1,3,2)
+plot(xplot,cdf('beta',xplot,beta_a,beta_b),'r','linewidth',3);
+hold on
+plot(xplot,cplot,'linewidth',3);
+title('Surrogate CDF')
+legend('True','Computed')
+hold off
+
+% Evaluate and plot the surrogate PDF
+pplot = pdf_eval(xplot);
+subplot(1,3,3)
+plot(xplot,pdf('beta',xplot,beta_a,beta_b),'r','linewidth',3);
+hold on
+plot(xplot,pplot,'linewidth',3);
+title('Surrogate PDF')
+legend('True','Computed')
+hold off
+
+pause
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Below is a 2D example for creating an EDF response surface
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Create some random samples from a 2D standard normal
+N = 400;
 x = randn(N,2);
-xe = pick2Dpoints(min(x(:))*[1 1],max(x(:))*[1 1],[30;30]);
-Fhat = @(xe,x) reshape(sum(all(repmat(x,[1,1,size(xe,1)])<=repmat(reshape(xe',[1,2,size(xe,1)]),[size(x,1),1,1]),2),1),size(xe,1),1)/size(x,1);
+
+% Evaluate the EDF at some points to create data for the model
+Ndata = 20;
+xdata = pick2Dpoints(min(x(:))*[1 1],max(x(:))*[1 1],Ndata*[1;1]);
+ydata = Fhat(xdata,x);
+
+% Plot the EDF from the randomly generated data
+h_cdf_ex = figure;
+subplot(1,3,1)
+surf(reshape(xdata(:,1),Ndata,Ndata),reshape(xdata(:,2),Ndata,Ndata),reshape(ydata,Ndata,Ndata))
+title('Empirical CDF')
+
+% Choose an RBF to work with
+rbf = rbfM6;
+rbfdxdy = rbfM6dxdy;
+
+% Create a surrogate model for the EDF
+ep = [1,1];
+mu = 4e-2;
+K_cdf = rbf(DistanceMatrix(xdata,xdata,ep));
+% cdf_coef = K_cdf\ydata;
+cdf_coef = (K_cdf+mu*eye(Ndata^2))\ydata;
+% cdf_coef = (K_cdf'*K_cdf+mu*eye(Ndata^2))\K_cdf'*ydata;
+cdf_eval = @(xeval) rbf(DistanceMatrix(xeval,xdata,ep))*cdf_coef;
+pdf_eval = @(xeval) max(rbfdxdy(DistanceMatrix(xeval,xdata,ep),DifferenceMatrix(xeval(:,1),xdata(:,1)),...
+                             DifferenceMatrix(xeval(:,2),xdata(:,2)),ep)*cdf_coef,0);
+
+% Evaluate and plot the surrogate CDF on a grid
+Nplot = 40;
+xplot = pick2Dpoints(min(x(:))*[1 1],max(x(:))*[1 1],Nplot*[1;1]);
+cplot = cdf_eval(xplot);
+subplot(1,3,2)
+surf(reshape(xplot(:,1),Nplot,Nplot),reshape(xplot(:,2),Nplot,Nplot),reshape(cplot,Nplot,Nplot))
+title('Surrogate CDF')
+
+% Evaluate and plot the surrogate PDF on a grid
+pplot = pdf_eval(xplot);
+subplot(1,3,3)
+surf(reshape(xplot(:,1),Nplot,Nplot),reshape(xplot(:,2),Nplot,Nplot),reshape(pplot,Nplot,Nplot))
+title('Surrogate PDF')
+
+pause
 
 % Load, clean and scale the data
 load carsmall
@@ -41,18 +162,13 @@ end
 h_ecdf = figure;
 surf(reshape(x2d(:,1),N2d,N2d),reshape(x2d(:,2),N2d,N2d),reshape(ecdf2d,N2d,N2d))
 
-rbfM2 = @(r) (1+r).*exp(-r);
-rbfM2dxdy = @(r,dx,dy,ep) prod(ep.^2)*exp(-r).*dx.*dy./(r+eps);
-rbfM4 = @(r) (3+3*r+r.^2).*exp(-r);
-rbfM4dxdy = @(r,dx,dy,ep) prod(ep.^2)*exp(-r).*dx.*dy;
-rbfM6 = @(r) (15+15*r+6*r.^2+r.^3).*exp(-r);
-rbfM6dxdy = @(r,dx,dy,ep) prod(ep.^2)*exp(-r).*dx.*dy.*(1+r);
 rbf = rbfM6;
 rbfdxdy = rbfM6dxdy;
 
 ep = [3,3];
+mu = 1e-3;
 K_cdf = rbf(DistanceMatrix(x2d,x2d,ep));
-cdf2d_coef = K_cdf\ecdf2d;
+cdf2d_coef = (K_cdf+mu*eye(N2d^2))\ecdf2d;
 cdf2d_eval = @(xx) rbf(DistanceMatrix(xx,x2d,ep))*cdf2d_coef;
 pdf2d_eval = @(xx) max(rbfdxdy(DistanceMatrix(xx,x2d,ep),DifferenceMatrix(xx(:,1),x2d(:,1)),...
                              DifferenceMatrix(xx(:,2),x2d(:,2)),ep)*cdf2d_coef,0);
