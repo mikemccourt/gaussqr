04ea7e76e6da6449f8bbacddc7a3f83fd7748d9d
Michael McCourt <mccomic@iit.edu>
2015-01-12 13:10:13 -0600

    Fixed the GSSEx1 code to look better and work correctly Need to decide if gssfunc is still required

--- book/GSSEx1.m
+++ book/GSSEx1.m
@@ -1,50 +1,82 @@
-f = @(x) sin(2*pi*x) + x;
-N = 10;
-x = pickpoints(0,1,N);
-x = pickpoints(0,1,N+2);x = x(2:end-1);
-y = f(x);
-NN = 150;
-xx = pickpoints(0,1,NN);
-yy = f(xx);
+% A demonstration of the computation of GSS kernels for a given inner
+% product.  This requires either the solution of a nonlinear or linear
+% system and there may be multiple solutions that produce the same kernel.
+%
+% There may also be some discussion of the reproducing property but at the
+% moment, that portion of the code below has been commented out.
 
-MM = @(x,z) {repmat(x,1,size(z,1)),repmat(z',size(x,1),1)};
-tmp = MM(x,x);
-X = tmp{1};
-Z = tmp{2};
-tmp = MM(xx,x);
-XX = tmp{1};
-ZZ = tmp{2};
+% The values in this vector allow are the coefficients of the inner
+% product which are chosen to define a specific kernel
+% I make this a column vector for later computation
+p = [101;100;102];
 
-% KK{1} = @(x,z) min(x,z) - .129*x.*z   - .3261*(x + z)  + .2656;
-% KK{2} = @(x,z) min(x,z) - 0.3598*x.*z - 0.2055*(x + z) + 0.1376;
-% KK{3} = @(x,z) min(x,z) + 3.3171*x.*z - 2.1692*(x + z) + 1.1006;
-% KK{4} = @(x,z) min(x,z) + .998*x.*z   - .9988*(x + z)  + .9999;
-
-KK{1} = @(x,z) min(x,z) - .4708*x.*z   - .3015*(x + z)  + .1690;
-KK{2} = @(x,z) min(x,z) - 0.5051*x.*z - 0.1616*(x + z) + 0.0884;
-KK{3} = @(x,z) min(x,z) + .2*x.*z - .4*(x + z) + .2;
-KK{4} = @(x,z) min(x,z) + .0119*x.*z   - 1.017*(x + z)  + 1.029;
-
-h = figure;
-for k=1:4
-    Kf = KK{k};
-    K_int = Kf(X,Z);
-    K_eval = Kf(XX,ZZ);
-    yp = K_eval*(K_int\y);
-    
-    subplot(2,2,k)
-    plot(xx,yp,'linewidth',3)
-    title(sprintf('Error = %g',errcompute(yp,yy)))
+% This test confirms that the values in this inner product actually produce
+% a valid semi-norm
+if not((p(1)+p(3)>=0) && (p(2)^2-p(1)*p(3)<=0))
+    error('The proposed inner product is invalid:\np(1)+p(3)=%g, p(2)^2-p(1)*p(3)=%g',p(1)+p(3),p(2)^2-p(1)*p(3))
 end
 
-pause
-%%%%%% Something below regarding reproducing property - more to do
-f = @(x) x.^2;
-fp = @(x) 2*x;
+% Form the linear system which defines the kernel coefficients
+% Note that this can be done linearly only because this particular problem
+% happens to be simple, and in general will require a nonlinear solve
+Ap = @(p) [0,p(2)-1,p(2)+p(1);...
+           0,p(3)+1,p(2)+p(3);...
+           p(2)-1,p(2)+p(1),0;...
+           p(3)+1,p(2)+p(3),0];
+
+% Using the linear system, solve for the kernel coefficients
+% This is a function that can be used to solve for any given p
+rf = @(p) Ap(p)\[1;0;-p(2);-p(3)];
+
+% Evaluate the kernel using known coefficients
+Kr = @(x,z,r) min(x,z) + r(1)*x.*z + r(2)*(x+z) + r(3);
+
+% Plot the kernel with a variety of centers
+x = pickpoints(0,1,500);
+r = rf(p);
+plot(x,[Kr(x,0,r),Kr(x,.2,r),Kr(x,.4,r),Kr(x,.6,r),Kr(x,.8,r),Kr(x,1,r)],'linewidth',3)
+fprintf('Inner product coefficients p=[%g %g %g]\n',p)
+fprintf('Kernel coefficients r=[%g %g %g]\n',r)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Below, we test if the same solution can be achieved with the nonlinear
+% solution strategy as with the linear solution strategy above.
+% Define the nonlinear components of the problem, noting that there are 4
+% here: ca = [c_1 c_2 a_1 a_2]
+ca_comp = @(ca) [(1-ca(1)^2)/ca(3) + ca(1)^2/ca(4) - 1;...
+                 ca(1)*ca(2)*(1/ca(3)-1/ca(4)) + 1;...
+                 ca(1)^2/ca(3) + (1-ca(1)^2)/ca(4) - 1;...
+                 ca(1)^2+ca(2)^2];
+
+% Define the full nonlinear function, including the nonhomogeneous RHS
+% This is where we use the fact that p is a column vector
+ca_solve = @(ca,p) ca_comp(ca) - [p;1];
+
+% Solve the nonlinear system, using a dumb initial guess
+init_guess = [2;2;2;2];
+fsolve_opt = optimoptions('fsolve','Display','off','MaxFunEvals',10000);
+ca = fsolve(@(ca)ca_solve(ca,p),init_guess,fsolve_opt);
+fprintf('Kernel c & a (nonlinear) coefficients c=[%g %g] a=[%g %g]\n',ca)
+
+% Define the kernel in terms of the ca values instead of the p values
+Kca = @(x,z,ca) min(x,z) + ...
+                x.*z*(-1+ca(3)+ca(4)+2*ca(1)*ca(2)*(ca(4)-ca(3))) + ...
+                (x+z)*(ca(1)*ca(2)*(ca(3)-ca(4))-ca(3)+ca(1)^2*(ca(3)-ca(4))) + ...
+                ca(3)+ca(1)^2*(ca(4)-ca(3));
+
+% Test if the kernel evaluated with this strategy and with the linear
+% strategy are the same kernels
+norm_diff = errcompute(Kca(x,.23456,ca),Kr(x,.23456,r));
+fprintf('Norm of difference between linear and nonlinear solves: %g\n',norm_diff)
+            
+return
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% Something below regarding reproducing property - NOT WORKING
 Ap = @(p) [0,p(2)-1,p(2)+p(1);0,p(3)+1,p(2)+p(3);p(2)-1,p(2)+p(1),0;p(3)+1,p(2)+p(3),0];
 rf = @(p) Ap(p)\[1;0;-p(2);-p(3)];
 p = [7,1,2];r=rf(p),norm(Ap(p)*r-[1;0;-p(2);-p(3)])
-Kr = @(x,z,r) min(x,z) + r(1)*x.*z + r(2)*(x+z) + r(3);
+f = @(x) x.^2;
+fp = @(x) 2*x;
 x = pickpoints(0,1,500);p = [7,1,2];r=rf(p);figure,plot(x,[Kr(x,0,r),Kr(x,.2,r),Kr(x,.4,r),Kr(x,.6,r),Kr(x,.8,r),Kr(x,1,r)],'linewidth',2)
 Krp = @(x,z,r) (x<z) + r(1)*z + r(2);
 ipP = @(fp,gp) quadl(@(x)fp(x).*gp(x),0,1);
@@ -61,6 +93,8 @@ ipB_known(f,z,r,p)
 zz = pickpoints(0,1,200)';
 fz = [];for z=zz fz = [fz ipP_known(f,z,r) + ipB_known(f,z,rf(p),p)];end
 figure,plot(zz,fz)
+
+pause
 
 fr = @(x) [-1+x(3)+x(4)+2*x(1)*x(2)*(x(4)-x(3));x(1)*x(2)*(x(3)-x(4))-x(3)+x(1)^2*(x(3)-x(4));x(3)+x(1)^2*(x(4)-x(3));x(1)^2+x(2)^2];
 v = fsolve(@(x)fr(x)-[r;1],[1;1;1;1])
---
+++ book/GSSEx1a.m
@@ -0,0 +1,43 @@
+% GSSEx1a.m
+% This script considers the plotting the interpolants of different GSS
+% kernels for missing boundary data, where the actions around the boundary
+% are determined by the optimality condition satisfied by the kernel.
+
+f = @(x) sin(2*pi*x) + x;
+N = 10;
+x = pickpoints(0,1,N);
+x = pickpoints(0,1,N+2);x = x(2:end-1);
+y = f(x);
+NN = 150;
+xx = pickpoints(0,1,NN);
+yy = f(xx);
+
+MM = @(x,z) {repmat(x,1,size(z,1)),repmat(z',size(x,1),1)};
+tmp = MM(x,x);
+X = tmp{1};
+Z = tmp{2};
+tmp = MM(xx,x);
+XX = tmp{1};
+ZZ = tmp{2};
+
+% KK{1} = @(x,z) min(x,z) - .129*x.*z   - .3261*(x + z)  + .2656;
+% KK{2} = @(x,z) min(x,z) - 0.3598*x.*z - 0.2055*(x + z) + 0.1376;
+% KK{3} = @(x,z) min(x,z) + 3.3171*x.*z - 2.1692*(x + z) + 1.1006;
+% KK{4} = @(x,z) min(x,z) + .998*x.*z   - .9988*(x + z)  + .9999;
+
+KK{1} = @(x,z) min(x,z) - .4708*x.*z   - .3015*(x + z)  + .1690;
+KK{2} = @(x,z) min(x,z) - 0.5051*x.*z - 0.1616*(x + z) + 0.0884;
+KK{3} = @(x,z) min(x,z) + .2*x.*z - .4*(x + z) + .2;
+KK{4} = @(x,z) min(x,z) + .0119*x.*z   - 1.017*(x + z)  + 1.029;
+
+h = figure;
+for k=1:4
+    Kf = KK{k};
+    K_int = Kf(X,Z);
+    K_eval = Kf(XX,ZZ);
+    yp = K_eval*(K_int\y);
+    
+    subplot(2,2,k)
+    plot(xx,yp,'linewidth',3)
+    title(sprintf('Error = %g',errcompute(yp,yy)))
+end
