fe43e3e48501c9fc667955ae6c62ee555ad895b7
Michael McCourt <mccomic@iit.edu>
2015-01-25 15:42:37 -0600

    Brought DifferenceMatrix up to par

--- source/utilities/DifferenceMatrix.m
+++ source/utilities/DifferenceMatrix.m
@@ -1,20 +1,24 @@
-% DM = DifferenceMatrix(datacoord,centercoord)
+function DiffM = DifferenceMatrix(xcoord,zcoord)
+% function DiffM = DifferenceMatrix(datacoord,centercoord)
 % Borrowed from Fasshauer's "Meshfree Approximation Methods in Matlab"
 %
 % Forms the difference matrix of two sets of points in R
 % (some fixed coordinate of point in R^s), i.e.,
 % DM(j,k) = datacoord_j - centercoord_k .
-  function DM = DifferenceMatrix(datacoord,centercoord)
-  % The ndgrid command produces two MxN matrices:
-  %   dr, consisting of N identical columns
-  %       (each containing the M data sites)
-  %   cc, consisting of M identical rows
-  %       (each containing the N centers)
-  
-  % Test to allow for user to pass [] and return []
-  if (length(datacoord)==0 || length(centercoord)==0)
-      DM = zeros(size(datacoord,1),size(centercoord,1));
-  else
-      [dr,cc] = ndgrid(datacoord(:),centercoord(:));
-      DM = dr-cc;
-  end
+%
+% You cannot call this with d-dimensional points - you can only call this
+% to compute the difference between a single dimension
+
+[N,xd] = size(xcoord);
+[M,zd] = size(zcoord);
+
+% First returns an empty matrix, for degenerate data
+% Then confirms that the user passed a column of data, not all dimensions
+% If possible, computes the difference matrix
+if (isempty(xcoord) || isempty(zcoord))
+    DiffM = zeros(N,M);
+elseif xd~=1 || zd~=1
+    error('Only 1 dimension at a time can be differenced: x dim=%d, z dim=%d',xd,zd)
+else
+    DiffM = bsxfun(@minus,xcoord,zcoord');
+end
