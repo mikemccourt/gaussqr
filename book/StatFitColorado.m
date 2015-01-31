44dc708e3c22c3a7c63ded9071b1e18a7b77d2f8
Michael McCourt <mccomic@iit.edu>
2015-01-31 15:08:27 -0600

    Finished Colorado river data study

--- book/StatFitColorado.m
+++ book/StatFitColorado.m
@@ -14,6 +14,8 @@
 % actually stored as [longitude,latitude] to match our normal orientation
 % of x & y in mathematics
 
+h_waitbar = waitbar(0,'Initializing','Visible','on');
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % First, we must grab the data, which is stored on the GaussQR server
 % We thank the USGS for making this data available to us
@@ -36,10 +38,10 @@ rbf = @(e,r) exp(-(e*r));
 
 % Choose a range of epsilon and mu parameters to consider
 epvec = logspace(-1,1,20);
-muvec = logspace(-6,-2,15);
+muvec = logspace(-4,-1.6,28);
 
 % Choose locations at which to make predictions
-Neval = 60;
+Neval = 61;
 xeval = pick2Dpoints(-1,1,Neval);
 
 % Define a function which makes the Kriging predictions
@@ -50,7 +52,9 @@ sf = @(ep,mu,x,y,xeval) rbf(ep,DistanceMatrix(xeval,x))*...
 cv = @(ep,mu) crossval('mse',x,y,...
                        'Predfun',@(x,y,xeval) sf(ep,mu,x,y,xeval),...
                        'leaveout',1);
-                
+
+waitbar(0,h_waitbar,'Creating \mu=0 CV plot');
+
 % Evaluate the cross-validation for our epsilon values
 cvepvec = arrayfun(@(ep) cv(ep,0),epvec);
 
@@ -60,28 +64,94 @@ semilogx(epvec,cvepvec,'linewidth',2)
 xlabel('$\varepsilon$','interpreter','latex')
 ylabel('mean square LOOCV residual')
 ylim([350 400])
+xlim([.1 10])
+
+waitbar(0,h_waitbar,'Finding optimal \mu=0 \epsilon');
 
 % Find the LOOCV minimizing epsilon and evaluate with it
 epopt = fminbnd(@(ep) cv(ep,0),1,2);
 seval = sf(ep,0,x,y,xeval);
+
+waitbar(0,h_waitbar,'Plotting optimal \mu=0 \epsilon surface');
 
 % Plot the results
 X1 = reshape(xeval(:,1),Neval,Neval);
 X2 = reshape(xeval(:,2),Neval,Neval);
 S = reshape(seval,Neval,Neval);
 h = figure;
-h_surf = surf(X1,X2,S,'edgealpha',.6);
+surf(X1,X2,S,'edgealpha',.6);
 hold on
-h_dots = plot3(x(:,1),x(:,2),y,'ok'); % The given data
+plot3(x(:,1),x(:,2),y,'or'); % The given data
 hold off
-view([-152 42])
-xlabel('longitude')
-ylabel('latitude')
-zlabel('Ferric Oxide percentage')
+view([189 22])
+xlabel('longitude (scaled)')
+ylabel('latitude (scaled)')
+zlabel('Lanthanum ppm')
 
 % Simplest possible plotting of the data
 h_griddata = figure;
 [X,Y] = meshgrid(pickpoints(-1,1,50),pickpoints(-1,1,50));
 L = griddata(x(:,1),x(:,2),y,X,Y);
 surf(X,Y,L),hold on,plot3(x(:,1),x(:,2),y,'og'),hold off
-view([-152 42])
+view([189 22])
+
+% Consider the optimization involving both the mu and ep values
+waitbar(0,h_waitbar,'Optimizing both \mu and \epsilon');
+
+cvmat = zeros(length(muvec),length(epvec));
+k = 1;
+for mu = muvec
+    cvmat(k,:) = arrayfun(@(ep) cv(ep,mu),epvec);
+    waitbar(k/length(muvec),h_waitbar,sprintf('\\mu=%3.2e',mu));
+    k = k + 1;
+end
+
+waitbar(1,h_waitbar,'Plotting two parameter CV surface');
+
+figure,h_cv2d = axes;
+[E,M] = meshgrid(epvec,muvec);
+C = cvmat;
+h_emsurface = surf(E,M,C);
+set(h_cv2d,'xscale','log')
+set(h_cv2d,'yscale','log')
+set(h_cv2d,'zscale','log')
+set(h_cv2d,'ylim',[1e-4,10^-1.6])
+set(h_cv2d,'zlim',[100,20000])
+set(h_cv2d,'clim',[350 450])
+xlabel('$\varepsilon$','interpreter','latex')
+ylabel('\mu')
+zlabel('mse LOOCV residual')
+view([-79.5 42])
+
+waitbar(1,h_waitbar,'Checking for optimal ep and mu values');
+
+logepmuopt_sm = fminunc(@(epmu) cv(exp(epmu(1)),exp(epmu(2))),log([3,1e-3]));
+logepmuopt_lg = fminunc(@(epmu) cv(exp(epmu(1)),exp(epmu(2))),log([1,1e-2]));
+
+waitbar(1,h_waitbar,'Plotting optimal ep and mu values');
+
+seval = sf(exp(logepmuopt_sm(1)),exp(logepmuopt_sm(2)),x,y,xeval);
+S = reshape(seval,Neval,Neval);
+h_optsm = figure;
+surf(X1,X2,S,'edgealpha',.6);
+hold on
+plot3(x(:,1),x(:,2),y,'or'); % The given data
+hold off
+view([189 22])
+xlabel('longitude (scaled)')
+ylabel('latitude (scaled)')
+zlabel('Lanthanum ppm')
+
+seval = sf(exp(logepmuopt_lg(1)),exp(logepmuopt_lg(2)),x,y,xeval);
+S = reshape(seval,Neval,Neval);
+h_optlg = figure;
+surf(X1,X2,S,'edgealpha',.6);
+hold on
+plot3(x(:,1),x(:,2),y,'or'); % The given data
+hold off
+view([189 22])
+xlabel('longitude (scaled)')
+ylabel('latitude (scaled)')
+zlabel('Lanthanum ppm')
+
+close(h_waitbar)
