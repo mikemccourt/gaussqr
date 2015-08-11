1d3df4874e944d83e5cd000ee08c2735318700cd
Michael McCourt <mccomic@iit.edu>
2015-08-11 14:22:01 -0400

    Small addition to an example

--- examples/approximation/ex6.m
+++ examples/approximation/ex6.m
@@ -9,8 +9,10 @@
 global GAUSSQR_PARAMETERS
 Mextramax = GAUSSQR_PARAMETERS.MAX_EXTRA_EFUNC;
 
+h_waitbar = waitbar(0,'Initializing');
+
 Nrange = 10:10:500;
-NN = 1000;
+Neval = 200;
 ep = 1;
 alpha = 1;
 yf = @(x) cos(x)+exp(-(x-1).^2)+exp(-(x+1).^2);
@@ -21,15 +23,19 @@ for i=1:length(Mfrac)
     for j=1:length(Nrange)
         N = Nrange(j);
         M = round(N*Mfrac(i));
+        waitbar(((i-1)*length(Nrange)+j)/(length(Nrange)*length(Mfrac)),h_waitbar,sprintf('N=%d, M=%d',N,M));
+        
         x = pickpoints(-3,3,N);
         y = yf(x);
-        xx = pickpoints(-3,3,NN);
-        yy = yf(xx);
+        xeval = pickpoints(-3,3,Neval,'halt');
+        yeval = yf(xeval);
         RBFQR = gqr_rsolve(x,y,ep,alpha,M);
-        yp = gqr_eval(RBFQR,xx);
-        errs(i,j) = errcompute(yp,yy);
+        yp = gqr_eval(RBFQR,xeval);
+        errs(i,j) = errcompute(yp,yeval);
     end
 end
+
+waitbar(1,h_waitbar,'Plotting')
 
 semilogy(Nrange,errs)
 title(sprintf('\\alpha=%g, \\epsilon=%g',alpha,ep))
@@ -37,3 +43,5 @@ legend('M=.1N','M=.2N','M=.3N','M=.4N','M=.5N','Location','NorthEast')
 xlabel('N')
 ylabel('Average error')
 ylim([1e-16 1])
+
+close(h_waitbar)
